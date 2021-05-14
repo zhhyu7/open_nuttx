79ff4a996353f6b73f2ec5dea6e5ef886fe1fd70
chao.an <anchao@xiaomi.com>
2021-05-14 11:54:20 +0800

    sim/bringup: enable uart bridge
    Change-Id: Ia9e6eee69a55da582e0b3159f80511a249041425

--- boards/sim/sim/sim/src/sim_bringup.c
+++ boards/sim/sim/sim/src/sim_bringup.c
@@ -39,6 +39,7 @@
 #include <nuttx/wireless/pktradio.h>
 #include <nuttx/wireless/bluetooth/bt_null.h>
 #include <nuttx/wireless/bluetooth/bt_uart_shim.h>
+#include <nuttx/wireless/bluetooth/bt_uart_bridge.h>
 #include <nuttx/wireless/ieee802154/ieee802154_loopback.h>
 #include <nuttx/i2c/i2c_master.h>
 #include <nuttx/sensors/mpu60x0.h>
@@ -364,6 +365,17 @@ int sim_bringup(void)
       syslog(LOG_ERR, "ERROR: btuart_register() failed: %d\n", ret);
     }
 #  endif
+#endif
+
+#ifdef CONFIG_BLUETOOTH_UART_BRIDGE
+  /* Register the Bluetooth BT/BLE dual mode bridge driver */
+
+  ret = bt_uart_bridge_register("/dev/ttyHCI",
+                                "/dev/ttyBT", "/dev/ttyBLE");
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "ERROR: bt_uart_bridge_register() failed: %d\n", ret);
+    }
 #endif
 
 #ifdef CONFIG_SIM_I2CBUS
