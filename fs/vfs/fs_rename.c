4566b392e7e450770119fc3afdad86b8a7e02db4
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-07-09 15:46:02 +0800

    fs: pseudorename should call inode_release in the error path
    Change-Id: I0fc64e2447cad2773c522af0724bda0b1691174b

--- fs/vfs/fs_rename.c
+++ fs/vfs/fs_rename.c
@@ -88,6 +88,7 @@ next_subdir:
 
       if (oldinode == newinode)
         {
+          inode_release(newinode);
           ret = OK;
           goto errout; /* Bad naming, this is not an error case. */
         }
@@ -111,6 +112,8 @@ next_subdir:
         {
           FAR char *subdirname;
           FAR char *tmp;
+
+          inode_release(newinode);
 
           /* Yes.. In this case, the target of the rename must be a
            * subdirectory of newinode, not the newinode itself.  For
