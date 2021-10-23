0bcfcd2125c31b993e0d5c07c8e9aae9aac6aed6
songnannan <songnannan@xiaomi.com>
2021-10-23 19:01:04 +0800

    feature: sensor: add polling mode for barometer lps27hhw.
    Change-Id: I883a55ff576910f87087704512e4d56ebe5dc927

--- drivers/leds/userled_lower.c
+++ drivers/leds/userled_lower.c
@@ -40,14 +40,10 @@
 
 static userled_set_t
 userled_supported(FAR const struct userled_lowerhalf_s *lower);
-static void userled_setled(FAR const struct userled_lowerhalf_s *lower,
-                           int led, bool ledon);
-static void userled_setall(FAR const struct userled_lowerhalf_s *lower,
+static void userled_led(FAR const struct userled_lowerhalf_s *lower,
+                        int led, bool ledon);
+static void userled_ledset(FAR const struct userled_lowerhalf_s *lower,
                            userled_set_t ledset);
-#ifdef CONFIG_USERLED_LOWER_READSTATE
-static void userled_getall(FAR const struct userled_lowerhalf_s *lower,
-                           userled_set_t *ledset);
-#endif
 
 /****************************************************************************
  * Private Data
@@ -60,11 +56,8 @@ static uint32_t g_lednum;
 static const struct userled_lowerhalf_s g_userled_lower =
 {
   .ll_supported = userled_supported,
-  .ll_setled    = userled_setled,
-  .ll_setall    = userled_setall,
-#ifdef CONFIG_USERLED_LOWER_READSTATE
-  .ll_getall    = userled_getall,
-#endif
+  .ll_led       = userled_led,
+  .ll_ledset    = userled_ledset,
 };
 
 /****************************************************************************
@@ -87,48 +80,32 @@ userled_supported(FAR const struct userled_lowerhalf_s *lower)
 }
 
 /****************************************************************************
- * Name: userled_setled
+ * Name: userled_led
  *
  * Description:
  *   Set the current state of one LED
  *
  ****************************************************************************/
 
-static void userled_setled(FAR const struct userled_lowerhalf_s *lower,
-                           int led, bool ledon)
+static void userled_led(FAR const struct userled_lowerhalf_s *lower,
+                        int led, bool ledon)
 {
   board_userled(led, ledon);
 }
 
 /****************************************************************************
- * Name: userled_setall
+ * Name: userled_led
  *
  * Description:
  *   Set the state of all LEDs
  *
  ****************************************************************************/
 
-static void userled_setall(FAR const struct userled_lowerhalf_s *lower,
+static void userled_ledset(FAR const struct userled_lowerhalf_s *lower,
                            userled_set_t ledset)
 {
   board_userled_all(ledset);
 }
-
-#ifdef CONFIG_USERLED_LOWER_READSTATE
-/****************************************************************************
- * Name: userled_getall
- *
- * Description:
- *   Get the state of all LEDs
- *
- ****************************************************************************/
-
-static void userled_getall(FAR const struct userled_lowerhalf_s *lower,
-                           userled_set_t *ledset)
-{
-  board_userled_getall(ledset);
-}
-#endif
 
 /****************************************************************************
  * Public Functions
