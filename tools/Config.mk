50a4b0fe813c47c0ef02c1dc64b8c0fb529c6171
weizhifang <weizhifang@xiaomi.com>
2021-10-25 17:28:38 +0800

    feature: charge: add battery charger da9168 driver
    Change-Id: I30e60e69ca3386b3916b1714b7a657bf6c0c3517

--- tools/Config.mk
+++ tools/Config.mk
@@ -337,9 +337,7 @@ endef
 # created from scratch
 
 define ARCHIVE
-	@echo "AR (create): ${shell basename $(1)} $(2)"
-	$(Q) $(RM) $1
-	$(Q) $(AR) $1 $(2)
+	$(AR) $1 $(2)
 endef
 
 # PRELINK - Prelink a list of files
@@ -460,6 +458,10 @@ define RWILDCARD
 endef
 
 # CLEAN - Default clean target
+
+ifeq ($(CONFIG_ARCH_COVERAGE),y)
+	OBJS += *.gcno *.gcda
+endif
 
 ifeq ($(CONFIG_WINDOWS_NATIVE),y)
 define CLEAN
