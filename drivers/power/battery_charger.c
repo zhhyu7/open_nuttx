f1334f421dcf3e37eed25fd0f80e4cd9770ae6cc
weizhifang <weizhifang@xiaomi.com>
2021-10-27 14:39:20 +0800

    feature: charge: add add chipid ioctl api
    Change-Id: I2cfe530ba87105e6b25ed0e666613d8e7305063f

--- drivers/power/battery_charger.c
+++ drivers/power/battery_charger.c
@@ -370,16 +370,6 @@ static int bat_charger_ioctl(FAR struct file *filep, int cmd,
         }
         break;
 
-      case BATIOC_GET_VOLTAGE:
-        {
-          FAR int *outvoltsp = (FAR int *)((uintptr_t)arg);
-          if (outvoltsp)
-            {
-              ret = dev->ops->get_voltage(dev, outvoltsp);
-            }
-        }
-        break;
-
       default:
         _err("ERROR: Unrecognized cmd: %d\n", cmd);
         ret = -ENOTTY;
