f098e51606270105a6ff927e87aa48ea64d701c8
Alan C. Assis <acassis@gmail.com>
2021-12-02 10:53:19 -0300

    Add WiFi/BLE Coexistence support
    Co-authored-by: Gustavo Henrique Nihei <gustavo.nihei@espressif.com>
    Co-authored-by: Abdelatif Guettouche <abdelatif.guettouche@espressif.com>
    Change-Id: I0a1a5dc2f6a62c828c235b2b777010962725381c

--- arch/arm/src/armv8-m/arm_backtrace.c
+++ arch/arm/src/armv8-m/arm_backtrace.c
@@ -81,13 +81,11 @@ static bool in_code_region(FAR void *pc)
 {
   int i = 0;
 
-#if 0
   if ((uintptr_t)pc >= (uintptr_t)_START_TEXT &&
       (uintptr_t)pc <  (uintptr_t)_END_TEXT)
     {
       return true;
     }
-#endif
 
   if (g_backtrace_code_regions)
     {
@@ -283,17 +281,12 @@ int up_backtrace(FAR struct tcb_s *tcb, FAR void **buffer, int size)
     }
   else
     {
-      ret = 0;
-
       flags = enter_critical_section();
 
-      buffer[ret++] = tcb->xcp.regs[REG_PC];
-      buffer[ret++] = tcb->xcp.regs[REG_LR];
-
-      ret += backtrace_branch(tcb->stack_base_ptr +
-                              tcb->adj_stack_size,
-                              (FAR void *)tcb->xcp.regs[REG_SP],
-                              &buffer[ret], size - ret);
+      ret = backtrace_branch(tcb->stack_base_ptr +
+                             tcb->adj_stack_size,
+                             (FAR void *)tcb->xcp.regs[REG_SP],
+                             buffer, size);
 
       leave_critical_section(flags);
     }
