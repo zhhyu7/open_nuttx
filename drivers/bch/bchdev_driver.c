e5d99d08cb7c8123be97a5de1b4041cfd4811c75
Huang Qi <huangqi3@xiaomi.com>
2021-12-27 00:18:22 +0800

    arch/risc-v: Refine Toolchain.defs
    Change-Id: Ia8670d9b52875966f4700f030c76501cd2758b3a

--- drivers/bch/bchdev_driver.c
+++ drivers/bch/bchdev_driver.c
@@ -223,7 +223,7 @@ static off_t bch_seek(FAR struct file *filep, off_t offset, int whence)
   FAR struct inode *inode = filep->f_inode;
   FAR struct bchlib_s *bch;
   off_t newpos;
-  off_t ret;
+  int ret;
 
   DEBUGASSERT(inode && inode->i_private);
 
@@ -231,7 +231,7 @@ static off_t bch_seek(FAR struct file *filep, off_t offset, int whence)
   ret = bchlib_semtake(bch);
   if (ret < 0)
     {
-      return ret;
+      return (off_t)ret;
     }
 
   /* Determine the new, requested file position */
