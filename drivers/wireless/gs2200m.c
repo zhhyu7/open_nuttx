ae4f66fdf91f678d544b1055b21acd866363db38
Gustavo Henrique Nihei <gustavo.nihei@espressif.com>
2022-03-10 20:30:01 -0300

    xtensa/esp32s3: Add support for RT-Timer based on Systimer peripheral
    Change-Id: I4d1ca168ebfbf0836acc12bf848acca1b1dbc51c
    Signed-off-by: Gustavo Henrique Nihei <gustavo.nihei@espressif.com>

--- drivers/wireless/gs2200m.c
+++ drivers/wireless/gs2200m.c
@@ -315,7 +315,7 @@ static uint8_t _cid_to_uint8(char c)
 static void _to_ascii_char(uint16_t num, char *str)
 {
   DEBUGASSERT(num <= 2032); /* See Table 20 */
-  snprintf(str, 5, "%04d", num);
+  snprintf(str, 6, "%04d", num);
 }
 
 /****************************************************************************
@@ -524,9 +524,18 @@ static void _check_pkt_q_empty(FAR struct gs2200m_dev_s *dev, char cid)
 
       while (pkt_dat)
         {
-          wlerr("=== error: found (type=%d msg[0]=%s|)\n",
-                pkt_dat->type, pkt_dat->msg[0]);
+          wlerr("=== error: found (cid=%c type=%d msg[0]=%s|)\n",
+                cid, pkt_dat->type, pkt_dat->msg[0]);
           pkt_dat = (FAR struct pkt_dat_s *)pkt_dat->dq.flink;
+
+          if (_cid_is_set(&dev->valid_cid_bits, cid))
+            {
+              wlerr("+++ error: cid=%c is still active !!!\n", cid);
+            }
+
+          /* NOTE: force to disable the cid to remove */
+
+          _enable_cid(&dev->valid_cid_bits, cid, false);
         }
 
       _remove_all_pkt(dev, c);
@@ -1543,7 +1552,7 @@ retry_recv:
       /* release & initialize pkt_dat before retry */
 
       _release_pkt_dat(dev, pkt_dat);
-      memset(pkt_dat, 0, sizeof(pkt_dat));
+      memset(pkt_dat, 0, sizeof(*pkt_dat));
 
       bulk = true;
       goto retry_recv;
@@ -1558,7 +1567,7 @@ retry_recv:
           /* release & initialize pkt_dat before retry */
 
           _release_pkt_dat(dev, pkt_dat);
-          memset(pkt_dat, 0, sizeof(pkt_dat));
+          memset(pkt_dat, 0, sizeof(*pkt_dat));
         }
 
       wlwarn("*** retry cmd=%s (n=%d)\n", cmd, n);
@@ -1630,7 +1639,7 @@ static enum pkt_type_e gs2200m_set_opmode(FAR struct gs2200m_dev_s *dev,
 static enum pkt_type_e gs2200m_get_mac(FAR struct gs2200m_dev_s *dev)
 {
   struct pkt_dat_s pkt_dat;
-  enum pkt_type_e   r;
+  enum pkt_type_e  r;
   uint32_t mac[6];
   char cmd[16];
   int n;
@@ -1722,7 +1731,7 @@ static enum pkt_type_e gs2200m_join_network(FAR struct gs2200m_dev_s *dev,
                                             FAR char *ssid, uint8_t ch)
 {
   struct pkt_dat_s pkt_dat;
-  enum pkt_type_e   r;
+  enum pkt_type_e  r;
   char cmd[64];
   char addr[3][17];
   int n;
@@ -1855,7 +1864,7 @@ static enum pkt_type_e gs2200m_set_wpa2pf(FAR struct gs2200m_dev_s *dev,
 enum pkt_type_e gs2200m_get_wstatus(FAR struct gs2200m_dev_s *dev)
 {
   struct pkt_dat_s pkt_dat;
-  enum pkt_type_e   r;
+  enum pkt_type_e  r;
   int i;
 
   /* Initialize pkt_dat and send command */
@@ -1888,8 +1897,8 @@ gs2200m_create_clnt(FAR struct gs2200m_dev_s *dev,
                     FAR struct gs2200m_connect_msg *msg,
                     FAR char *cid)
 {
-  enum pkt_type_e  r;
   struct pkt_dat_s pkt_dat;
+  enum pkt_type_e  r;
   char cmd[40];
   char *p;
   int   n;
@@ -1953,8 +1962,8 @@ static enum pkt_type_e gs2200m_start_server(FAR struct gs2200m_dev_s *dev,
                                             FAR char *port, bool is_tcp,
                                             FAR char *cid)
 {
-  enum pkt_type_e   r;
   struct pkt_dat_s pkt_dat;
+  enum pkt_type_e  r;
   char cmd[40];
   char *p;
   int   n;
@@ -2000,10 +2009,10 @@ errout:
 static enum pkt_type_e gs2200m_send_bulk(FAR struct gs2200m_dev_s *dev,
                                          FAR struct gs2200m_send_msg *msg)
 {
-  enum pkt_type_e   r;
   enum spi_status_e s;
+  enum pkt_type_e   r;
   int  bulk_hdr_size;
-  char digits[5];
+  char digits[6];
   char cmd[32];
 
   memset(cmd, 0, sizeof(cmd));
@@ -2173,7 +2182,7 @@ static enum pkt_type_e gs2200m_get_cstatus(FAR struct gs2200m_dev_s *dev,
                                            FAR struct gs2200m_name_msg *msg)
 {
   struct pkt_dat_s pkt_dat;
-  enum pkt_type_e   r;
+  enum pkt_type_e  r;
   char cmd[16];
   int i;
 
@@ -2536,13 +2545,20 @@ static int gs2200m_ioctl_accept(FAR struct gs2200m_dev_s *dev,
   uint8_t c;
   char s_cid;
   char c_cid;
+  int ret = OK;
   int n;
 
   wlinfo("+++ start: cid=%c\n", msg->cid);
 
   c = _cid_to_uint8(msg->cid);
   pkt_dat = (FAR struct pkt_dat_s *)dq_peek(&dev->pkt_q[c]);
-  ASSERT(pkt_dat);
+
+  if (NULL == pkt_dat)
+    {
+      wlerr("*** error: cid=%c not found\n", msg->cid);
+      ret = -EINVAL;
+      goto errout;
+    }
 
   n = sscanf(pkt_dat->msg[0], "CONNECT %c %c", &s_cid, &c_cid);
   ASSERT(2 == n);
@@ -2574,13 +2590,20 @@ static int gs2200m_ioctl_accept(FAR struct gs2200m_dev_s *dev,
   nmsg.local = 0;
   nmsg.cid = msg->cid;
   r = gs2200m_get_cstatus(dev, &nmsg);
-  ASSERT(TYPE_OK == r);
+
+  if (TYPE_OK != r)
+    {
+      wlerr("*** error: cid=%c not found\n", msg->cid);
+      ret = -EINVAL;
+      goto errout;
+    }
 
   msg->addr = nmsg.addr;
 
+errout:
   wlinfo("+++ end: type=%d (msg->cid=%c)\n", msg->type, msg->cid);
 
-  return OK;
+  return ret;
 }
 
 /****************************************************************************
@@ -2745,7 +2768,7 @@ static int gs2200m_ioctl_iwreq(FAR struct gs2200m_dev_s *dev,
 {
   struct iwreq *res = (struct iwreq *)&msg->ifr;
   struct pkt_dat_s pkt_dat;
-  enum pkt_type_e   r;
+  enum pkt_type_e  r;
   char cmd[64];
   char cmd2[64];
   int  n = 0;
