498e7cd91f10b87c239412dc0b7881a4193af101
Abdelatif Guettouche <abdelatif.guettouche@espressif.com>
2022-04-20 23:50:12 +0200

    arch/xtensa: Replace the xcp context with stack context to improve context switching
    Change-Id: I74949450bdf15119ea749ea1dea3ff2aa41f4e8d
    Signed-off-by: Abdelatif Guettouche <abdelatif.guettouche@espressif.com>

--- boards/arm/stm32/stm3210e-eval/src/stm32_pmbuttons.c
+++ boards/arm/stm32/stm3210e-eval/src/stm32_pmbuttons.c
@@ -96,6 +96,8 @@
 #  define CONFIG_PM_BUTTON_ACTIVITY 10
 #endif
 
+#define PM_IDLE_DOMAIN 0 /* Revisit */
+
 /* Miscellaneous Definitions ************************************************/
 
 #ifndef MIN
