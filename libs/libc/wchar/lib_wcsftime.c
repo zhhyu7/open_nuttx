03f11be6b2794c2c4a6f09d2a7bcc9ff7605c804
Abdelatif Guettouche <abdelatif.guettouche@espressif.com>
2022-06-13 14:19:46 +0200

    arch/xtensa: Refactor exceptions' entry and exit.
    Change-Id: If5502e614499e176db6d098c003e7af644695e17
    Signed-off-by: Abdelatif Guettouche <abdelatif.guettouche@espressif.com>

--- libs/libc/wchar/lib_wcsftime.c
+++ libs/libc/wchar/lib_wcsftime.c
@@ -22,12 +22,14 @@
  * Included Files
  ****************************************************************************/
 
+#include <nuttx/config.h>
 #include <time.h>
 
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
 
+#ifdef CONFIG_LIBC_WCHAR
 size_t wcsftime(FAR wchar_t *s, size_t maxsize, FAR const wchar_t *format,
                 FAR const struct tm *tim_p)
 {
@@ -37,3 +39,4 @@ size_t wcsftime(FAR wchar_t *s, size_t maxsize, FAR const wchar_t *format,
 
   return strftime((FAR char *)s, maxsize, (FAR char *)format, tim_p);
 }
+#endif
