74046f01b4a9cab4871b41273aa1e0f87e5fbaf1
Ville Juven <ville.juven@unikie.com>
2022-11-23 11:26:08 +0200

    obstack/obstack_free: Fix incorrect usage of void* for arithmetics
    Using void* for arithmetics is a GCC extension, and thus should not be
    used for portable code.
    
    Change-Id: Ie88d8fa020fef2bffd4fccd8521d8976b50a6f72

--- mm/mempool/mempool_multiple.c
+++ mm/mempool/mempool_multiple.c
@@ -32,7 +32,6 @@
 #define SIZEOF_HEAD sizeof(FAR struct mempool_s *)
 #define MAX(a, b)   ((a) > (b) ? (a) : (b))
 #define MIN(a, b)   ((a) < (b) ? (a) : (b))
-#define ALIGN_BIT   (1 << 1)
 
 /****************************************************************************
  * Private Functions
@@ -45,17 +44,10 @@ mempool_multiple_find(FAR struct mempool_multiple_s *mpool, size_t size)
   size_t left = 0;
   size_t mid;
 
-  if (mpool->delta != 0)
-    {
-      left = mpool->pools[0].blocksize;
-      mid = (size - left + mpool->delta - 1) / mpool->delta;
-      return mid < right ? &mpool->pools[mid] : NULL;
-    }
-
   while (left < right)
     {
       mid = (left + right) >> 1;
-      if (mpool->pools[mid].blocksize > size)
+      if (mpool->pools[mid].bsize > size)
         {
           right = mid;
         }
@@ -83,12 +75,9 @@ mempool_multiple_find(FAR struct mempool_multiple_s *mpool, size_t size)
  * Description:
  *   Initialize multiple memory pool, each element represents a memory pool.
  *   The user needs to specify the initialization information of each mempool
- *   in the array, including blocksize, initialsize, expandsize,
- *   interruptsize, wait. These mempool will be initialized by mempool_init.
- *   The name of all mempool are "name".
- *
- *   This function will initialize the member delta by detecting the
- *   relationship between the each block size of mempool in multiple mempool.
+ *   in the array, including bsize, ninitial, nexpand, ninterrupt, wait.
+ *   These mempool will be initialized by mempool_init. The name of all
+ *   mempool are "name".
  *
  * Input Parameters:
  *   name  - The name of memory pool.
@@ -105,20 +94,6 @@ int mempool_multiple_init(FAR struct mempool_multiple_s *mpool,
   int i;
 
   DEBUGASSERT(mpool != NULL && mpool->pools != NULL);
-
-  mpool->delta = 0;
-  for (i = 1; i < mpool->npools; i++)
-    {
-      size_t delta = mpool->pools[i].blocksize -
-                     mpool->pools[i - 1].blocksize;
-      if (mpool->delta != 0 && delta != mpool->delta)
-        {
-          mpool->delta = 0;
-          break;
-        }
-
-      mpool->delta = delta;
-    }
 
   for (i = 0; i < mpool->npools; i++)
     {
@@ -215,19 +190,7 @@ FAR void *mempool_multiple_realloc(FAR struct mempool_multiple_s *mpool,
 
       oldpool = *(FAR struct mempool_s **)
                 ((FAR char *)oldblk - SIZEOF_HEAD);
-      if ((uintptr_t)oldpool & ALIGN_BIT)
-        {
-          oldpool = (FAR struct mempool_s *)
-                    ((uintptr_t)oldpool & ~ALIGN_BIT);
-          size = MIN(size, oldpool->blocksize -
-                     *(FAR size_t *)((FAR char *)oldblk - 2 * SIZEOF_HEAD));
-        }
-      else
-        {
-          size = MIN(size, oldpool->blocksize - SIZEOF_HEAD);
-        }
-
-      memcpy(blk, oldblk, size);
+      memcpy(blk, oldblk, MIN(oldpool->bsize, size));
       mempool_multiple_free(mpool, oldblk);
     }
 
@@ -250,18 +213,12 @@ void mempool_multiple_free(FAR struct mempool_multiple_s *mpool,
                            FAR void *blk)
 {
   FAR struct mempool_s *pool;
-  FAR char *mem;
+  FAR void *mem;
 
   DEBUGASSERT(mpool != NULL && blk != NULL);
 
   mem = (FAR char *)blk - SIZEOF_HEAD;
   pool = *(FAR struct mempool_s **)mem;
-  if ((uintptr_t)pool & ALIGN_BIT)
-    {
-      pool = (FAR struct mempool_s *)((uintptr_t)pool & ~ALIGN_BIT);
-      mem = (FAR char *)blk - *(FAR size_t *)(mem - SIZEOF_HEAD);
-    }
-
   mempool_free(pool, mem);
 }
 
@@ -282,126 +239,13 @@ void mempool_multiple_free(FAR struct mempool_multiple_s *mpool,
 size_t mempool_multiple_alloc_size(FAR void *blk)
 {
   FAR struct mempool_s *pool;
-  FAR char *mem;
+  FAR void *mem;
 
   DEBUGASSERT(blk != NULL);
 
   mem = (FAR char *)blk - SIZEOF_HEAD;
   pool = *(FAR struct mempool_s **)mem;
-  if ((uintptr_t)pool & ALIGN_BIT)
-    {
-      pool = (FAR struct mempool_s *)((uintptr_t)pool & ~ALIGN_BIT);
-      return pool->blocksize - *(FAR size_t *)(mem - SIZEOF_HEAD);
-    }
-  else
-    {
-      return pool->blocksize - SIZEOF_HEAD;
-    }
-}
-
-/****************************************************************************
- * Name: mempool_multiple_memalign
- *
- * Description:
- *   This function requests more than enough space from malloc, finds a
- *   region within that chunk that meets the alignment request.
- *
- *   The alignment argument must be a power of two.
- *
- *   The memalign is special to multiple mempool because multiple mempool
- *   doesn't support split and shrink chunk operate. So When you alloc a
- *   memory block and find an aligned address in this block, you need to
- *   occupy 8 bytes before the address to save the address of the padding
- *   size and pool to ensure correct use in realloc and free operations.
- *   So we will use bit1 in the previous address of the address to represent
- *   that it is applied by memalign.
- *
- * Input Parameters:
- *   mpool     - The handle of multiple memory pool to be used.
- *   alignment - The alignment request of memory block.
- *   size      - The size of alloc blk.
- *
- * Returned Value:
- *   The size of memory block.
- *
- ****************************************************************************/
-
-FAR void *mempool_multiple_memalign(FAR struct mempool_multiple_s *mpool,
-                                    size_t alignment, size_t size)
-{
-  FAR struct mempool_s *end = mpool->pools + mpool->npools;
-  FAR struct mempool_s *pool;
-
-  DEBUGASSERT((alignment & (alignment - 1)) == 0);
-
-  pool = mempool_multiple_find(mpool, size + alignment + 2 * SIZEOF_HEAD);
-  if (pool == NULL)
-    {
-      return NULL;
-    }
-
-  do
-    {
-      FAR char *blk = mempool_alloc(pool);
-      if (blk != NULL)
-        {
-          FAR char *mem;
-
-          mem = blk + 2 * SIZEOF_HEAD;
-          mem = (FAR char *)(((uintptr_t)mem + alignment - 1) &
-                             ~(alignment - 1));
-          *(FAR uintptr_t *)(mem - SIZEOF_HEAD) =
-                                                 (uintptr_t)pool | ALIGN_BIT;
-          *(FAR size_t *)(mem - 2 * SIZEOF_HEAD) = mem - blk;
-          return mem;
-        }
-    }
-  while (++pool < end);
-
-  return NULL;
-}
-
-/****************************************************************************
- * Name: mempool_multiple_info_task
- ****************************************************************************/
-
-void mempool_multiple_info_task(FAR struct mempool_multiple_s *mpool,
-                                FAR struct mempoolinfo_task *info)
-{
-  size_t i;
-
-  for (i = 0; i < mpool->npools; i++)
-    {
-      mempool_info_task(mpool->pools + i, info);
-    }
-}
-
-/****************************************************************************
- * Name: mempool_multiple_memdump
- *
- * Description:
- *   mempool_multiple_memdump returns a memory info about specified pid of
- *   task/thread. if pid equals -1, this function will dump all allocated
- *   node and output backtrace for every allocated node for this multiple
- *   mempool, if pid equals -2, this function will dump all free node for
- *   this multiple mempool, and if pid is greater than or equal to 0, will
- *   dump pid allocated node and output backtrace.
- *
- * Input Parameters:
- *   mpool - The handle of multiple memory pool to be used.
- *   pid   - The pid of task.
- *
- ****************************************************************************/
-
-void mempool_multiple_memdump(FAR struct mempool_multiple_s *mpool,
-                              pid_t pid)
-{
-  size_t i;
-
-  for (i = 0; i < mpool->npools; i++)
-    {
-      mempool_memdump(mpool->pools + i, pid);
-    }
+  return pool->bsize;
 }
 
 /****************************************************************************
