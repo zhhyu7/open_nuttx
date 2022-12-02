66af8410dd97883c0706c07b8923fb42776aa57e
anjiahao <anjiahao@xiaomi.com>
2022-12-02 16:09:33 +0800

    minidumpserver: add arm-a support
    Change-Id: I56fb931de823567aaa771a83fa21006c7d99f62b

--- arch/arm/src/common/arm_backtrace_sp.c
+++ arch/arm/src/common/arm_backtrace_sp.c
@@ -262,7 +262,7 @@ int up_backtrace(struct tcb_s *tcb,
               ret += backtrace_branch((unsigned long)
                                       rtcb->stack_base_ptr +
                                       rtcb->adj_stack_size,
-                                      CURRENT_REGS[REG_SP],
+                                      rtcb->xcp.regs[REG_SP],
                                       &buffer[ret],
                                       size - ret, &skip);
             }
