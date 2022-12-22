b176f85b4942cbf481c595a304061d189c30167a
Peter van der Perk <peter.vanderperk@nxp.com>
2022-12-22 14:08:45 +0100

    S32K3XX FlexCAN kconfig fix to work in conjunction with EMAC ioctl
    Change-Id: I51936163f242c82995982672be6fc2a9106d238b

--- arch/arm/src/s32k3xx/s32k3xx_flexcan.c
+++ arch/arm/src/s32k3xx/s32k3xx_flexcan.c
@@ -1664,7 +1664,7 @@ static int s32k3xx_txavail(struct net_driver_s *dev)
  *
  ****************************************************************************/
 
-#ifdef CONFIG_NETDEV_CAN_BITRATE_IOCTL
+#ifdef CONFIG_NETDEV_IOCTL
 static int s32k3xx_ioctl(struct net_driver_s *dev, int cmd,
                          unsigned long arg)
 {
@@ -1674,6 +1674,7 @@ static int s32k3xx_ioctl(struct net_driver_s *dev, int cmd,
 
   switch (cmd)
     {
+#ifdef CONFIG_NETDEV_CAN_BITRATE_IOCTL
       case SIOCGCANBITRATE: /* Get bitrate from a CAN controller */
         {
           struct can_ioctl_data_s *req =
@@ -1738,6 +1739,7 @@ static int s32k3xx_ioctl(struct net_driver_s *dev, int cmd,
             }
         }
         break;
+#endif
 
       default:
         ret = -ENOTTY;
