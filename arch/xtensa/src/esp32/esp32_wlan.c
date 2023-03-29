9b3b4723ac253af405c8b5430c98f5f49fbe19c2
Tiago Medicci Serrano <tiago.medicci@espressif.com>
2023-03-29 13:48:34 -0300

    esp32/wifi: fix typos, goto and macro definitions
    Change-Id: Ib9a9d0493b355d62c1c229ae763733acc7ad235d

--- arch/xtensa/src/esp32/esp32_wlan.c
+++ arch/xtensa/src/esp32/esp32_wlan.c
@@ -223,7 +223,7 @@ static const struct wlan_ops g_sta_ops =
   .event      = esp_wifi_notify_subscribe,
   .stop       = esp_wifi_sta_stop
 };
-#endif
+#endif /* ESP32_WLAN_HAS_STA */
 
 #ifdef ESP32_WLAN_HAS_SOFTAP
 static const struct wlan_ops g_softap_ops =
@@ -246,7 +246,7 @@ static const struct wlan_ops g_softap_ops =
   .event      = esp_wifi_notify_subscribe,
   .stop       = esp_wifi_softap_stop
 };
-#endif
+#endif /* ESP32_WLAN_HAS_SOFTAP */
 
 /****************************************************************************
  * Private Function Prototypes
@@ -1713,7 +1713,7 @@ static void wlan_sta_tx_done(uint8_t *data, uint16_t *len, bool status)
 
   wlan_tx_done(priv);
 }
-#endif
+#endif /* ESP32_WLAN_HAS_STA */
 
 /****************************************************************************
  * Function: wlan_softap_rx_done
@@ -1764,7 +1764,7 @@ static void wlan_softap_tx_done(uint8_t *data, uint16_t *len, bool status)
 
   wlan_tx_done(priv);
 }
-#endif
+#endif /* ESP32_WLAN_HAS_SOFTAP */
 
 /****************************************************************************
  * Public Functions
@@ -1828,7 +1828,7 @@ int esp32_wlan_sta_initialize(void)
 
   return OK;
 }
-#endif
+#endif /* ESP32_WLAN_HAS_STA */
 
 /****************************************************************************
  * Name: esp32_wlan_softap_initialize
@@ -1889,6 +1889,6 @@ int esp32_wlan_softap_initialize(void)
 
   return OK;
 }
-#endif
+#endif /* ESP32_WLAN_HAS_SOFTAP */
 
 #endif  /* CONFIG_ESP32_WIFI */
