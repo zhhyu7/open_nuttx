29293f3e78623ca71f52b8260d8a46585430a10f
Brennan Ashton <bashton@brennanashton.com>
2023-05-07 00:13:31 -0700

    fdt: Add initial FDT support and procfs for userspace export
    Change-Id: I6082f85f2961dd8f259b7bf234d394958ffa8f3f

--- arch/arm/src/armv8-m/arm_schedulesigaction.c
+++ arch/arm/src/armv8-m/arm_schedulesigaction.c
@@ -160,8 +160,9 @@ void up_schedule_sigaction(struct tcb_s *tcb, sig_deliver_t sigdeliver)
 #endif
               CURRENT_REGS[REG_XPSR]       = ARMV8M_XPSR_T;
 #ifdef CONFIG_BUILD_PROTECTED
-              CURRENT_REGS[REG_LR]         = EXC_RETURN_PRIVTHR;
-              CURRENT_REGS[REG_EXC_RETURN] = EXC_RETURN_PRIVTHR;
+              CURRENT_REGS[REG_LR]         = EXC_RETURN_THREAD;
+              CURRENT_REGS[REG_EXC_RETURN] = EXC_RETURN_THREAD;
+              CURRENT_REGS[REG_CONTROL]    = getcontrol() & ~CONTROL_NPRIV;
 #endif
             }
         }
@@ -208,7 +209,8 @@ void up_schedule_sigaction(struct tcb_s *tcb, sig_deliver_t sigdeliver)
 #endif
           tcb->xcp.regs[REG_XPSR]    = ARMV8M_XPSR_T;
 #ifdef CONFIG_BUILD_PROTECTED
-          tcb->xcp.regs[REG_LR]      = EXC_RETURN_PRIVTHR;
+          tcb->xcp.regs[REG_LR]      = EXC_RETURN_THREAD;
+          tcb->xcp.regs[REG_CONTROL] = getcontrol() & ~CONTROL_NPRIV;
 #endif
         }
     }
@@ -318,7 +320,8 @@ void up_schedule_sigaction(struct tcb_s *tcb, sig_deliver_t sigdeliver)
 #endif
                   tcb->xcp.regs[REG_XPSR]    = ARMV8M_XPSR_T;
 #ifdef CONFIG_BUILD_PROTECTED
-                  tcb->xcp.regs[REG_LR]      = EXC_RETURN_PRIVTHR;
+                  tcb->xcp.regs[REG_LR]      = EXC_RETURN_THREAD;
+                  tcb->xcp.regs[REG_CONTROL] = getcontrol() & ~CONTROL_NPRIV;
 #endif
                 }
               else
@@ -364,7 +367,8 @@ void up_schedule_sigaction(struct tcb_s *tcb, sig_deliver_t sigdeliver)
 #endif
                   CURRENT_REGS[REG_XPSR]    = ARMV8M_XPSR_T;
 #ifdef CONFIG_BUILD_PROTECTED
-                  CURRENT_REGS[REG_LR]      = EXC_RETURN_PRIVTHR;
+                  CURRENT_REGS[REG_LR]      = EXC_RETURN_THREAD;
+                  CURRENT_REGS[REG_CONTROL] = getcontrol() & ~CONTROL_NPRIV;
 #endif
                 }
 
@@ -430,7 +434,8 @@ void up_schedule_sigaction(struct tcb_s *tcb, sig_deliver_t sigdeliver)
 #endif
           tcb->xcp.regs[REG_XPSR]    = ARMV8M_XPSR_T;
 #ifdef CONFIG_BUILD_PROTECTED
-          tcb->xcp.regs[REG_LR]      = EXC_RETURN_PRIVTHR;
+          tcb->xcp.regs[REG_LR]      = EXC_RETURN_THREAD;
+          tcb->xcp.regs[REG_CONTROL] = getcontrol() & ~CONTROL_NPRIV;
 #endif
         }
     }
