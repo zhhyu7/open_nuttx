803f5ebf1c4370dbacf351da9fc72349a187ce2c
zhangyuan21 <zhangyuan21@xiaomi.com>
2023-06-27 10:40:06 +0800

    armv7-a/gicv2: move IRQ to group1 and group0 as the FIQ
    Change-Id: I964f06a74c09ecc9e4656f8d0c87f2c8c653098e

--- arch/arm/src/common/arm_internal.h
+++ arch/arm/src/common/arm_internal.h
@@ -403,6 +403,9 @@ int  arm_securefault(int irq, void *context, void *arg);
 /* Interrupt acknowledge and dispatch */
 
 uint32_t *arm_doirq(int irq, uint32_t *regs);
+#ifdef CONFIG_ARCH_HIPRI_INTERRUPT
+uint32_t *arm_dofiq(int fiq, uint32_t *regs);
+#endif
 
 /* Paging support */
 
