6e2d0efa6b6925436ff0d3a8c2691fa1738f4d98
zhanghongyu <zhanghongyu@xiaomi.com>
2023-09-19 17:52:34 +0800

    Revert "wifi: move netdev_carrier_on from event cb to set_ssid function"
    Change-Id: I743ffd3be7a070dff9d2bfeac983e9fbf2a939e1

--- drivers/wireless/ieee80211/bcm43xxx/bcmf_driver.c
+++ drivers/wireless/ieee80211/bcm43xxx/bcmf_driver.c
@@ -684,9 +684,16 @@ void bcmf_wl_auth_event_handler(FAR struct bcmf_dev_s *priv,
       carrier = 0;
     }
 
-  if (carrier == 0)
+  if (carrier >= 0)
     {
-      netdev_carrier_off(&priv->bc_dev);
+      if (carrier)
+        {
+          netdev_carrier_on(&priv->bc_dev);
+        }
+      else
+        {
+          netdev_carrier_off(&priv->bc_dev);
+        }
     }
 
   if (auth && priv->auth_signal)
@@ -2046,7 +2053,6 @@ int bcmf_wl_set_ssid(FAR struct bcmf_dev_s *priv, struct iwreq *iwr)
     {
       case OK:
         wlinfo("AP Join ok\n");
-        netdev_carrier_on(&priv->bc_dev);
         break;
 
       default:
