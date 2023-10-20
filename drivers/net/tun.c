23d94e47f770848fd931bbb7d0a30c828d0707d8
zhanghongyu <zhanghongyu@xiaomi.com>
2023-10-20 15:17:48 +0800

    tun: Fix the error of calling tun_close when tun_txavail or tun_txavail_work is executed
    Change-Id: I79bf30756463fd35426afe1c54148cd5d947aae8

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -775,13 +775,18 @@ static void tun_txavail_work(FAR void *arg)
 static int tun_txavail(FAR struct net_driver_s *dev)
 {
   FAR struct tun_device_s *priv = (FAR struct tun_device_s *)dev->d_private;
+  irqstate_t flags;
+
+  flags = enter_critical_section(); /* No interrupts */
 
   /* Schedule to perform the TX poll on the worker thread. */
 
-  if (work_available(&priv->work))
+  if (priv->bifup && work_available(&priv->work))
     {
       work_queue(TUNWORK, &priv->work, tun_txavail_work, priv, 0);
     }
+
+  leave_critical_section(flags);
 
   return OK;
 }
@@ -910,6 +915,8 @@ static void tun_dev_uninit(FAR struct tun_device_s *priv)
   /* Put the interface in the down state */
 
   tun_ifdown(&priv->dev);
+
+  work_cancel_sync(TUNWORK, &priv->work);
 
   /* Remove the device from the OS */
 
