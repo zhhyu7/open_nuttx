3c6fa766dc6c96d567a01dedca1636a544da9983
anjiahao <anjiahao@xiaomi.com>
2023-10-27 21:57:33 +0800

    sched/procfs:use instrument to statistics run time max stack
    Change-Id: If93d12d2486bab560ab691defe6128411960f114

--- sched/init/nx_start.c
+++ sched/init/nx_start.c
@@ -56,6 +56,7 @@
 #include "irq/irq.h"
 #include "group/group.h"
 #include "init/init.h"
+#include "instrument/instrument.h"
 #include "tls/tls.h"
 
 /****************************************************************************
@@ -566,6 +567,10 @@ void nx_start(void)
    */
 
   sched_lock();
+
+  /* Initialize the instrument function */
+
+  instrument_initialize();
 
   /* Initialize the file system (needed to support device drivers) */
 
