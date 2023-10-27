30c17e7949857e0cab26dc96ab788393f3d8d251
guohao15 <guohao15@xiaomi.com>
2023-10-27 15:29:41 +0800

    api:add lib_realpath function
    Change-Id: Ia1ba7d26cce0989779acc8929706b5d8995acb9b

--- include/nuttx/lib/lib.h
+++ include/nuttx/lib/lib.h
@@ -126,6 +126,11 @@ FAR char *lib_realpath(FAR const char *path, FAR char *resolved,
 FAR char *lib_get_pathbuffer(void);
 void lib_put_pathbuffer(FAR char *buffer);
 
+/* Functions defined in lib_realpath.c **************************************/
+
+FAR char *lib_realpath(FAR const char *path, FAR char *resolved,
+                       bool notfollow);
+
 #undef EXTERN
 #ifdef __cplusplus
 }
