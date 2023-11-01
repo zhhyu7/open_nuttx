8ce80e8b367d57c6856a892972a84badb33e6422
anjiahao <anjiahao@xiaomi.com>
2023-11-01 15:27:18 +0800

    arch:mark armv7-a key function noinstrument_function
    Change-Id: I6793b81b06fb4a76d3c2a66822722bf69a517f8c

--- arch/arm/include/irq.h
+++ arch/arm/include/irq.h
@@ -116,7 +116,7 @@ EXTERN volatile uint32_t *g_current_regs[CONFIG_SMP_NCPUS];
  ****************************************************************************/
 
 #ifdef CONFIG_SMP
-int up_cpu_index(void);
+int up_cpu_index(void) noinstrument_function ;
 #else
 #  define up_cpu_index() (0)
 #endif
