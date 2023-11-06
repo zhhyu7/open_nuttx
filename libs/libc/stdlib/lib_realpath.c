492b488471f10194f787c7fa1c7f3b4bd367d9e1
chao an <anchao@xiaomi.com>
2023-11-06 16:19:40 +0800

    libc/realpath: allocate link buffer of pseudofs to save stack
    Change-Id: I67c934a3454eaa574421403391d1d35e08e00f2c

--- libs/libc/stdlib/lib_realpath.c
+++ libs/libc/stdlib/lib_realpath.c
@@ -40,7 +40,10 @@ FAR char *lib_realpath(FAR const char *path, FAR char *resolved,
                        bool notfollow)
 {
 #ifdef CONFIG_PSEUDOFS_SOFTLINKS
-  char wbuf[2][PATH_MAX];
+  FAR char *wbuf[2] =
+    {
+    };
+
   int nlnk = 0;
   int idx = 0;
   ssize_t n;
@@ -118,6 +121,14 @@ loop:
         }
 
       *p = '\0';
+
+#ifdef CONFIG_PSEUDOFS_SOFTLINKS
+      if (wbuf[0] != NULL)
+        {
+          lib_free(wbuf[0]);
+        }
+#endif
+
       return resolved;
     }
 
@@ -194,7 +205,19 @@ loop:
           goto out;
         }
 
-      n = readlink(resolved, wbuf[idx], sizeof(wbuf[0]) - 1);
+      if (wbuf[0] == NULL)
+        {
+          wbuf[0] = lib_calloc(2, PATH_MAX);
+          if (wbuf[0] == NULL)
+            {
+              set_errno(ENOMEM);
+              goto out;
+            }
+
+          wbuf[1] = wbuf[0] + PATH_MAX;
+        }
+
+      n = readlink(resolved, wbuf[idx], PATH_MAX - 1);
       if (n <= 0)
         {
           if (n == 0)
@@ -207,7 +230,7 @@ loop:
 
       /* Append unresolved path to link target and switch to it. */
 
-      if (n + (len = strlen(q)) + 1 > sizeof(wbuf[0]))
+      if (n + (len = strlen(q)) + 1 > PATH_MAX)
         {
           set_errno(ENAMETOOLONG);
           goto out;
@@ -242,6 +265,13 @@ loop:
 
 out:
   lib_free(fres);
+#ifdef CONFIG_PSEUDOFS_SOFTLINKS
+  if (wbuf[0] != NULL)
+    {
+      lib_free(wbuf[0]);
+    }
+#endif
+
   return NULL;
 }
 
