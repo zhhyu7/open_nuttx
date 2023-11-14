e1fb0f83ebc9e3ac8b84c15b822774a9d0a60569
yinshengkai <yinshengkai@xiaomi.com>
2023-11-14 15:50:52 +0800

    note: add memory tracing event support
    Change-Id: I012414019cf9e0682d1f150af998769c0337bef1

--- mm/mm_heap/mm_malloc.c
+++ mm/mm_heap/mm_malloc.c
@@ -31,6 +31,7 @@
 #include <nuttx/arch.h>
 #include <nuttx/mm/mm.h>
 #include <nuttx/sched.h>
+#include <nuttx/sched_note.h>
 
 #include "mm_heap/mm.h"
 #include "kasan/kasan.h"
@@ -285,7 +286,8 @@ FAR void *mm_malloc(FAR struct mm_heap_s *heap, size_t size)
 
       /* Update heap statistics */
 
-      heap->mm_curused += MM_SIZEOF_NODE(node);
+      nodesize = MM_SIZEOF_NODE(node);
+      heap->mm_curused += nodesize;
       if (heap->mm_curused > heap->mm_maxused)
         {
           heap->mm_maxused = heap->mm_curused;
@@ -303,7 +305,8 @@ FAR void *mm_malloc(FAR struct mm_heap_s *heap, size_t size)
   if (ret)
     {
       MM_ADD_BACKTRACE(heap, node);
-      kasan_unpoison(ret, mm_malloc_size(heap, ret));
+      kasan_unpoison(ret, nodesize);
+      sched_note_heap(true, heap, ret, nodesize);
 #ifdef CONFIG_MM_FILL_ALLOCATIONS
       memset(ret, 0xaa, alignsize - MM_ALLOCNODE_OVERHEAD);
 #endif
