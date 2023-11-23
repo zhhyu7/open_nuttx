eb3ec1f0917b2a0a02e5c7e0feba7007357523f9
ligd <liguiding1@xiaomi.com>
2023-11-23 17:11:16 +0800

    local_socket: corrent send/recv return value after shutdown
    Change-Id: Ic43c62223d95095147374d0f973f422d16f4091a

--- net/local/local_sendmsg.c
+++ net/local/local_sendmsg.c
@@ -397,12 +397,20 @@ errout_with_halfduplex:
 ssize_t local_sendmsg(FAR struct socket *psock, FAR struct msghdr *msg,
                       int flags)
 {
+  FAR struct local_conn_s *conn = psock->s_conn;
   FAR const struct sockaddr *to = msg->msg_name;
   FAR const struct iovec *buf = msg->msg_iov;
   socklen_t tolen = msg->msg_namelen;
   size_t len = msg->msg_iovlen;
+
+  /* Check shutdown state */
+
+  if (conn->lc_outfile.f_inode == NULL)
+    {
+      return -EPIPE;
+    }
+
 #ifdef CONFIG_NET_LOCAL_SCM
-  FAR struct local_conn_s *conn = psock->s_conn;
   int count = 0;
 
   if (msg->msg_control &&
@@ -414,11 +422,10 @@ ssize_t local_sendmsg(FAR struct socket *psock, FAR struct msghdr *msg,
           return count;
         }
     }
-#endif /* CONFIG_NET_LOCAL_SCM */
 
   len = to ? local_sendto(psock, buf, len, flags, to, tolen) :
              local_send(psock, buf, len, flags);
-#ifdef CONFIG_NET_LOCAL_SCM
+
   if (len < 0 && count > 0)
     {
       net_lock();
@@ -432,6 +439,9 @@ ssize_t local_sendmsg(FAR struct socket *psock, FAR struct msghdr *msg,
 
       net_unlock();
     }
+#else
+  len = to ? local_sendto(psock, buf, len, flags, to, tolen) :
+             local_send(psock, buf, len, flags);
 #endif
 
   return len;
