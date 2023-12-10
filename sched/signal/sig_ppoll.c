4cc453ef6c82c5594908aa139851698085c5a700
hujun5 <hujun5@xiaomi.com>
2023-12-10 15:41:54 +0800

    sched/signal: There is no need to use sched_[un]lock
    Change-Id: Ibd9250828661f8405ed7c3dda6a3dc8bad57e10f

--- sched/signal/sig_ppoll.c
+++ sched/signal/sig_ppoll.c
@@ -124,7 +124,6 @@ int ppoll(FAR struct pollfd *fds, nfds_t nfds,
       /* We are running again, restore the original sigprocmask */
 
       rtcb->sigprocmask = saved_sigprocmask;
-      leave_critical_section(flags);
 
       /* Now, handle the (rare?) case where (a) a blocked signal was received
        * while the task was suspended but (b) restoring the original
@@ -132,6 +131,7 @@ int ppoll(FAR struct pollfd *fds, nfds_t nfds,
        */
 
       nxsig_unmask_pendingsignal();
+      leave_critical_section(flags);
     }
 
   return ret;
