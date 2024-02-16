6f9c10f060b20b3b6c31f442b9621c8e0a7d8b67
Xiang Xiao <xiaoxiang@xiaomi.com>
2024-02-16 14:15:30 +0800

    rpmsg/ping: Replace UINT_MAX with CLOCK_MAX
    Change-Id: I77a44ff466f29bacd23c533b57d84026f54b6ba9

--- drivers/rptun/rptun_ping.c
+++ drivers/rptun/rptun_ping.c
@@ -186,7 +186,7 @@ static void rptun_ping_logout_rate(uint64_t len, clock_t avg)
 int rptun_ping(FAR struct rpmsg_endpoint *ept,
                FAR const struct rptun_ping_s *ping)
 {
-  clock_t min = ULONG_MAX;
+  clock_t min = CLOCK_MAX;
   clock_t max = 0;
   uint64_t total = 0;
   uint32_t buf_len = 0;
