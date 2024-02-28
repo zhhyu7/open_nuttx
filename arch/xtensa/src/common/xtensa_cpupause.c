a64623b21d5d9af1caed11ee225c434cf6f8e57f
wanggang26 <wanggang26@xiaomi.com>
2024-02-28 11:56:18 +0800

    fs:fix a typo
    Change-Id: I7ce99a3f009655141e3cdc1f2a3f79d3c9f51d66

--- arch/xtensa/src/common/xtensa_cpupause.c
+++ arch/xtensa/src/common/xtensa_cpupause.c
@@ -98,7 +98,7 @@ bool up_cpu_pausereq(int cpu)
 
 int up_cpu_paused(int cpu)
 {
-  struct tcb_s *tcb = this_task();
+  struct tcb_s *tcb = this_task_inirq();
 
   /* Update scheduler parameters */
 
@@ -130,7 +130,7 @@ int up_cpu_paused(int cpu)
    * assigned task list.
    */
 
-  tcb = this_task();
+  tcb = this_task_inirq();
 
 #ifdef CONFIG_SCHED_INSTRUMENTATION
   /* Notify that we have resumed */
@@ -230,7 +230,7 @@ int up_cpu_pause(int cpu)
 #ifdef CONFIG_SCHED_INSTRUMENTATION
   /* Notify of the pause event */
 
-  sched_note_cpu_pause(this_task(), cpu);
+  sched_note_cpu_pause(this_task_inirq(), cpu);
 #endif
 
   DEBUGASSERT(cpu >= 0 && cpu < CONFIG_SMP_NCPUS && cpu != this_cpu());
@@ -301,7 +301,7 @@ int up_cpu_resume(int cpu)
 #ifdef CONFIG_SCHED_INSTRUMENTATION
   /* Notify of the resume event */
 
-  sched_note_cpu_resume(this_task(), cpu);
+  sched_note_cpu_resume(this_task_inirq(), cpu);
 #endif
 
   DEBUGASSERT(cpu >= 0 && cpu < CONFIG_SMP_NCPUS && cpu != this_cpu());
