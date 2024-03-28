3721354de5240332eedbf8ca0ff3a087d348a479
ligd <liguiding1@xiaomi.com>
2024-03-28 17:57:03 +0800

    stack: update up_get_intstackbase API to support cpu id
    Change-Id: I4a21f7c3333851fc454900a65812b20eadf9bebd

--- arch/arm64/src/common/arm64_initialize.c
+++ arch/arm64/src/common/arm64_initialize.c
@@ -108,9 +108,9 @@ INIT_STACK_DEFINE(g_interrupt_fiq_stack, INTSTACK_SIZE);
  ****************************************************************************/
 
 #ifdef CONFIG_SMP
-uintptr_t arm64_intstack_alloc(void)
+uintptr_t arm64_intstack_alloc(int cpu)
 {
-  return (uintptr_t)(g_interrupt_stacks[up_cpu_index()]);
+  return (uintptr_t)(g_interrupt_stacks[cpu]);
 }
 
 /****************************************************************************
@@ -122,9 +122,9 @@ uintptr_t arm64_intstack_alloc(void)
  *
  ****************************************************************************/
 
-uintptr_t arm64_intstack_top(void)
+uintptr_t arm64_intstack_top(int cpu)
 {
-  return (uintptr_t)(g_interrupt_stacks[up_cpu_index()] + INTSTACK_SIZE);
+  return (uintptr_t)(g_interrupt_stacks[cpu] + INTSTACK_SIZE);
 }
 
 #endif
