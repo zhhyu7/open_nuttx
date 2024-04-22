555275a49a79f6ae9edce1791b00169e16c5767b
raiden00pl <raiden00@railab.me>
2024-04-22 20:05:36 +0200

    sensors/bh1749nuc_uorb.c: fix compilation when poll interface is disabled
    Change-Id: I8b7fc1f76f00c5804609219181a52ba222eea51a

--- arch/tricore/include/irq.h
+++ arch/tricore/include/irq.h
@@ -61,9 +61,9 @@ extern "C"
  ****************************************************************************/
 
 /* g_current_regs[] holds a references to the current interrupt level
- * register storage structure.  If is non-NULL only during interrupt
+ * register storage structure.  It is non-NULL only during interrupt
  * processing.  Access to g_current_regs[] must be through the macro
- * g_current_regs for portability.
+ * CURRENT_REGS for portability.
  */
 
 /* For the case of architectures with multiple CPUs, then there must be one
@@ -71,6 +71,7 @@ extern "C"
  */
 
 EXTERN volatile uintptr_t *g_current_regs[CONFIG_SMP_NCPUS];
+#define CURRENT_REGS (g_current_regs[up_cpu_index()])
 
 /****************************************************************************
  * Public Function Prototypes
@@ -144,20 +145,6 @@ noinstrument_function static inline void up_irq_restore(irqstate_t flags)
 }
 
 /****************************************************************************
- * Inline Functions
- ****************************************************************************/
-
-static inline_function uintptr_t *up_current_regs(void)
-{
-  return (uintptr_t *)g_current_regs[up_cpu_index()];
-}
-
-static inline_function void up_set_current_regs(uintptr_t *regs)
-{
-  g_current_regs[up_cpu_index()] = regs;
-}
-
-/****************************************************************************
  * Name: up_interrupt_context
  *
  * Description:
@@ -167,13 +154,13 @@ static inline_function void up_set_current_regs(uintptr_t *regs)
  ****************************************************************************/
 
 noinstrument_function
-static inline_function bool up_interrupt_context(void)
+static inline bool up_interrupt_context(void)
 {
 #ifdef CONFIG_SMP
   irqstate_t flags = up_irq_save();
 #endif
 
-  bool ret = up_current_regs() != NULL;
+  bool ret = CURRENT_REGS != NULL;
 
 #ifdef CONFIG_SMP
   up_irq_restore(flags);
--- net/local/local_sendmsg.c
+++ net/local/local_sendmsg.c
@@ -210,7 +210,8 @@ static ssize_t local_send(FAR struct socket *psock,
               return ret;
             }
 
-          ret = local_send_packet(&conn->lc_outfile, buf, len);
+          ret = local_send_packet(&conn->lc_outfile, buf, len,
+                                  psock->s_type == SOCK_DGRAM);
           nxmutex_unlock(&conn->lc_sendlock);
         }
         break;
@@ -261,7 +262,7 @@ static ssize_t local_sendto(FAR struct socket *psock,
 {
 #ifdef CONFIG_NET_LOCAL_DGRAM
   FAR struct local_conn_s *conn = psock->s_conn;
-  FAR const struct sockaddr_un *unaddr = (FAR const struct sockaddr_un *)to;
+  FAR struct sockaddr_un *unaddr = (FAR struct sockaddr_un *)to;
   ssize_t ret;
 
   /* Verify that a valid address has been provided */
@@ -271,17 +272,6 @@ static ssize_t local_sendto(FAR struct socket *psock,
       nerr("ERROR: Unrecognized address family: %d\n",
            to->sa_family);
       return -EAFNOSUPPORT;
-    }
-
-  /* At present, only standard pathname type address are support */
-
-  if (tolen < sizeof(sa_family_t) + 2)
-    {
-      /* EFAULT
-       * - An invalid user space address was specified for a parameter
-       */
-
-      return -EFAULT;
     }
 
   /* If this is a connected socket, then return EISCONN */
@@ -306,26 +296,20 @@ static ssize_t local_sendto(FAR struct socket *psock,
       return -EISCONN;
     }
 
-  if (local_findconn(conn, unaddr) == NULL)
-    {
-      nerr("ERROR: No such file or directory\n");
-      return -ENOENT;
-    }
-
-  /* Make sure that dgram is sent safely */
-
-  ret = nxmutex_lock(&conn->lc_sendlock);
-  if (ret < 0)
-    {
-      /* May fail because the task was canceled. */
-
-      nerr("ERROR: Failed to get localsocket sendlock: %zd\n", ret);
-      return ret;
-    }
-
   /* The outgoing FIFO should not be open */
 
   DEBUGASSERT(conn->lc_outfile.f_inode == NULL);
+
+  /* At present, only standard pathname type address are support */
+
+  if (tolen < sizeof(sa_family_t) + 2)
+    {
+      /* EFAULT
+       * - An invalid user space address was specified for a parameter
+       */
+
+      return -EFAULT;
+    }
 
   /* Make sure that half duplex FIFO has been created.
    * REVISIT:  Or should be just make sure that it already exists?
@@ -335,8 +319,8 @@ static ssize_t local_sendto(FAR struct socket *psock,
   if (ret < 0)
     {
       nerr("ERROR: Failed to create FIFO for %s: %zd\n",
-           unaddr->sun_path, ret);
-      goto errout_with_lock;
+           conn->lc_path, ret);
+      return ret;
     }
 
   /* Open the sending side of the transfer */
@@ -352,23 +336,25 @@ static ssize_t local_sendto(FAR struct socket *psock,
       goto errout_with_halfduplex;
     }
 
-  /* Send the preamble */
+  /* Make sure that dgram is sent safely */
 
-  ret = local_send_preamble(conn, &conn->lc_outfile, buf, len);
+  ret = nxmutex_lock(&conn->lc_sendlock);
   if (ret < 0)
     {
-      nerr("ERROR: Failed to send the preamble: %zd\n", ret);
+      /* May fail because the task was canceled. */
+
       goto errout_with_sender;
     }
 
   /* Send the packet */
 
-  ret = local_send_packet(&conn->lc_outfile, buf, len);
+  ret = local_send_packet(&conn->lc_outfile, buf, len, true);
   if (ret < 0)
     {
       nerr("ERROR: Failed to send the packet: %zd\n", ret);
-      goto errout_with_sender;
     }
+
+  nxmutex_unlock(&conn->lc_sendlock);
 
 errout_with_sender:
 
@@ -382,12 +368,6 @@ errout_with_halfduplex:
   /* Release our reference to the half duplex FIFO */
 
   local_release_halfduplex(conn);
-
-errout_with_lock:
-
-  /* Release localsocket sendlock */
-
-  nxmutex_unlock(&conn->lc_sendlock);
   return ret;
 #else
   return -EISCONN;
@@ -436,19 +416,17 @@ ssize_t local_sendmsg(FAR struct socket *psock, FAR struct msghdr *msg,
           return count;
         }
     }
+#endif /* CONFIG_NET_LOCAL_SCM */
 
   len = to ? local_sendto(psock, buf, len, flags, to, tolen) :
              local_send(psock, buf, len, flags);
-
+#ifdef CONFIG_NET_LOCAL_SCM
   if (len < 0 && count > 0)
     {
       net_lock();
       local_freectl(conn, count);
       net_unlock();
     }
-#else
-  len = to ? local_sendto(psock, buf, len, flags, to, tolen) :
-             local_send(psock, buf, len, flags);
 #endif
 
   return len;
