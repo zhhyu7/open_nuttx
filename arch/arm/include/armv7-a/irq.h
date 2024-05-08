3dd68cf6930937adf3ccdbdd93cd4b6fa8940fbb
guoshichao <guoshichao@xiaomi.com>
2024-05-08 17:31:41 +0800

    armv7a/irq: enable fiq in tee, enable irq in ap
    Change-Id: I7e835c2fe94dd92dc6ea422e3bf8b3fc8c11b898

--- arch/arm/include/armv7-a/irq.h
+++ arch/arm/include/armv7-a/irq.h
@@ -405,7 +405,7 @@ noinstrument_function static inline irqstate_t up_irq_save(void)
     (
       "\tmrs    %0, cpsr\n"
 #ifdef CONFIG_ARCH_TRUSTZONE_SECURE
-      "\tcpsid  if\n"
+      "\tcpsid  f\n"
 #else
       "\tcpsid  i\n"
 #endif
@@ -426,8 +426,10 @@ static inline irqstate_t up_irq_enable(void)
   __asm__ __volatile__
     (
       "\tmrs    %0, cpsr\n"
-#if defined(CONFIG_ARCH_TRUSTZONE_SECURE) || defined(CONFIG_ARCH_HIPRI_INTERRUPT)
+#if defined(CONFIG_ARCH_HIPRI_INTERRUPT)
       "\tcpsie  if\n"
+#elif defined(CONFIG_ARCH_TRUSTZONE_SECURE)
+      "\tcpsie  f\n"
 #else
       "\tcpsie  i\n"
 #endif
