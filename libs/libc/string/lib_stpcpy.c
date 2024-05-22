35fa4bbca953ae127b626ec3a79b267201fda247
p-szafonimateusz <p-szafonimateusz@xiaomi.com>
2024-05-22 16:42:23 +0200

    libc/x86_64: port string functions from bionic
    Change-Id: Ic0648967a7907ffc68fc53ee39064a2b9e4a4c8e

--- libs/libc/string/lib_stpcpy.c
+++ libs/libc/string/lib_stpcpy.c
@@ -62,7 +62,7 @@
  *
  ****************************************************************************/
 
-#ifndef CONFIG_ARCH_STPCPY
+#ifndef CONFIG_LIBC_ARCH_STPCPY
 #undef stpcpy /* See mm/README.txt */
 nosanitize_address
 FAR char *stpcpy(FAR char *dest, FAR const char *src)
