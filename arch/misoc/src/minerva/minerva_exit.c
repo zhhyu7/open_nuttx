9d5500ac8cace17a90c8863c4d013f87649de6f7
p-szafonimateusz <p-szafonimateusz@xiaomi.com>
2024-05-22 16:42:23 +0200

    libc/x86_64: port string functions from bionic
    Change-Id: I95d0edf72260338bcc04babc07d7ce18dc6f1d2f

--- arch/misoc/src/minerva/minerva_exit.c
+++ arch/misoc/src/minerva/minerva_exit.c
@@ -54,17 +54,13 @@
 
 void up_exit(int status)
 {
-  struct tcb_s *tcb;
+  struct tcb_s *tcb = this_task();
 
   /* Make sure that we are in a critical section with local interrupts.  The
    * IRQ state will be restored when the next task is started.
    */
 
   enter_critical_section();
-
-  sinfo("TCB=%p exiting\n", this_task_irq());
-
-  nxsched_dumponexit();
 
   /* Destroy the task at the head of the ready to run list. */
 
@@ -74,7 +70,7 @@ void up_exit(int status)
    * of the list.
    */
 
-  tcb = this_task_irq();
+  tcb = this_task();
 
   /* Adjusts time slice for SCHED_RR & SCHED_SPORADIC cases */
 
