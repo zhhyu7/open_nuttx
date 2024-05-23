e4f48e61b2cdd4f80a84846f9efb9412ec20026b
shizhenghui <shizhenghui@xiaomi.com>
2024-05-23 21:00:56 +0800

    sim codec: move host src into posix directory
    Change-Id: I18ec4458c6b0c03098957fdd3a30b1c66f1f3abe

--- arch/sim/src/sim/posix/sim_hostdecoder.c
+++ arch/sim/src/sim/posix/sim_hostdecoder.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * arch/sim/src/sim/sim_hostdecoder.c
+ * arch/sim/src/sim/posix/sim_hostdecoder.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
--- arch/sim/src/sim/posix/sim_hostencoder.c
+++ arch/sim/src/sim/posix/sim_hostencoder.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * arch/sim/src/sim/sim_hostencoder.c
+ * arch/sim/src/sim/posix/sim_hostencoder.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
