a0eb8665f4b3800be203a065eb629e9eeaa1c1a2
chenxiaoyi <chenxiaoyi@xiaomi.com>
2024-06-13 11:49:24 +0800

    types.h: fix windows build error
    Change-Id: I6f216d8b1d250ca8482f92a9af3ff1bf0dca7503

--- arch/risc-v/include/types.h
+++ arch/risc-v/include/types.h
@@ -78,6 +78,9 @@ typedef __WCHAR_TYPE__     _wchar_t;
 typedef int                _wchar_t;
 #endif
 
+typedef int                _wint_t;
+typedef int                _wctype_t;
+
 #ifdef CONFIG_ARCH_RV64
 /* A size is 8 bytes */
 
