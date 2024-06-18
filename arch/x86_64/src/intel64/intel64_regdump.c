f37a40bf2e2a3052d9712fdc2f7eaf907f5d9494
liwenxiang1 <liwenxiang1@xiaomi.com>
2024-06-18 11:33:11 +0800

    arch/x86_64:Add FP Backtrace function
    Change-Id: Ib9d8cb4431ea3032008aabf9bc2bd352426735d8

--- arch/x86_64/src/intel64/intel64_regdump.c
+++ arch/x86_64/src/intel64/intel64_regdump.c
@@ -151,19 +151,5 @@ void up_dump_register(void *dumpregs)
   _alert("R14: %016" PRIx64 ", R15: %016" PRIx64 "\n",
          regs[REG_R14], regs[REG_R15]);
   _alert("Dumping Stack (+-64 bytes):\n");
-
-  if (regs[REG_RSP] > 0 && regs[REG_RSP] < 0x1000000)
-    {
-      print_mem((void *)regs[REG_RSP] - 512,
-          128 * 0x200000 - regs[REG_RSP] + 512);
-    }
-  else
-    {
-      print_mem((void *)regs[REG_RSP] - 512, 1024);
-    }
-
-#ifdef CONFIG_DEBUG_NOOPT
-  backtrace(regs[REG_RBP]);
-#endif
   _alert("-----------------------------------------\n");
 }
