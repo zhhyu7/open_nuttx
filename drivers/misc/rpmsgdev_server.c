d5b0017fba5a90040fe30a12bba29f673bf35d36
wangjianyu3 <wangjianyu3@xiaomi.com>
2024-07-09 17:12:24 +0800

    rpmsgdev_server: Support oneway polling
    Change-Id: Ia095eae956ce170ea12ccfc6128a06b588ac9534

--- drivers/misc/rpmsgdev_server.c
+++ drivers/misc/rpmsgdev_server.c
@@ -370,7 +370,14 @@ static int rpmsgdev_poll_handler(FAR struct rpmsg_endpoint *ept,
     {
       /* Do not allow double setup */
 
-      DEBUGASSERT(dev->cfd == 0);
+      if (dev->cfd != 0)
+        {
+          msg->header.result = file_poll(&dev->file, &dev->fd, false);
+          if (msg->header.result < 0)
+            {
+              return rpmsg_send(ept, msg, len);
+            }
+        }
 
       dev->cfd        = msg->fds;
       dev->fd.events  = msg->events;
@@ -382,6 +389,8 @@ static int rpmsgdev_poll_handler(FAR struct rpmsg_endpoint *ept,
     }
   else
     {
+      DEBUGASSERT(dev->cfd != 0);
+
       msg->header.result = file_poll(&dev->file, &dev->fd, false);
       if (msg->header.result == OK)
         {
@@ -389,7 +398,7 @@ static int rpmsgdev_poll_handler(FAR struct rpmsg_endpoint *ept,
         }
     }
 
-  return rpmsg_send(ept, msg, len);
+  return msg->header.cookie ? rpmsg_send(ept, msg, len) : OK;
 }
 
 /****************************************************************************
