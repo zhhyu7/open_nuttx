3f8edc12e90e92630f8f9e2a43fc4ff6f1ea857b
Xiang Xiao <xiaoxiang@xiaomi.com>
2024-07-28 15:29:22 +0800

    fs/romfs: Change the type of num from uint8_t to uint16_t
    Change-Id: Iee9b75121a0f0bad4f3cb6f01b44eb4391ebf018

--- fs/romfs/fs_romfsutil.c
+++ fs/romfs/fs_romfsutil.c
@@ -604,7 +604,7 @@ static int romfs_cachenode(FAR struct romfs_mountpt_s *rm,
   FAR struct romfs_nodeinfo_s **child;
   FAR struct romfs_nodeinfo_s *nodeinfo;
   char childname[NAME_MAX + 1];
-  uint8_t num = 0;
+  uint16_t count = 0;
   uint32_t info;
   size_t nsize;
   int ret;
@@ -669,11 +669,11 @@ static int romfs_cachenode(FAR struct romfs_mountpt_s *rm,
           return ret;
         }
 
-      if (child == NULL || nodeinfo->rn_count == num - 1)
+      if (child == NULL || nodeinfo->rn_count == count - 1)
         {
           FAR void *tmp;
 
-          tmp = kmm_realloc(nodeinfo->rn_child, (num + NODEINFO_NINCR) *
+          tmp = kmm_realloc(nodeinfo->rn_child, (count + NODEINFO_NINCR) *
                             sizeof(*nodeinfo->rn_child));
           if (tmp == NULL)
             {
@@ -681,9 +681,9 @@ static int romfs_cachenode(FAR struct romfs_mountpt_s *rm,
             }
 
           nodeinfo->rn_child = tmp;
-          memset(nodeinfo->rn_child + num, 0, NODEINFO_NINCR *
+          memset(nodeinfo->rn_child + count, 0, NODEINFO_NINCR *
                   sizeof(*nodeinfo->rn_child));
-          num += NODEINFO_NINCR;
+          count += NODEINFO_NINCR;
         }
 
       child = &nodeinfo->rn_child[nodeinfo->rn_count++];
