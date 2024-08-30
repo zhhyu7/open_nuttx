1e27b130735c781ea33dc05c5d455378ee669472
zhangyuan29 <zhangyuan29@xiaomi.com>
2024-08-30 13:52:26 +0800

    Revert "armv6-m: change privileged mode use msp"
    Change-Id: Ibe6b9d83eb23ffff38253d5c7d2b69e503e2de3b

--- arch/arm/src/armv6-m/arm_schedulesigaction.c
+++ arch/arm/src/armv6-m/arm_schedulesigaction.c
@@ -162,8 +162,8 @@ void up_schedule_sigaction(struct tcb_s *tcb, sig_deliver_t sigdeliver)
           tcb->xcp.regs[REG_PRIMASK]    = 1;
           tcb->xcp.regs[REG_XPSR]       = ARMV6M_XPSR_T;
 #ifdef CONFIG_BUILD_PROTECTED
-          tcb->xcp.regs[REG_LR]         = EXC_RETURN_PRIVTHR;
-          tcb->xcp.regs[REG_EXC_RETURN] = EXC_RETURN_PRIVTHR;
+          tcb->xcp.regs[REG_LR]         = EXC_RETURN_THREAD;
+          tcb->xcp.regs[REG_EXC_RETURN] = EXC_RETURN_THREAD;
 #endif
 
 #ifdef CONFIG_SMP
