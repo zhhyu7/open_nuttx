ac9ad7cf5d0996324074cb9a0e4be1b795aaf7a9
Xiang Xiao <xiaoxiang@xiaomi.com>
2024-10-07 14:36:37 +0800

    netlink: Remove the dead code in netlink_close
    Change-Id: Ie6f728bc404ee139e99b23dde3121e1e745e019f

--- net/netlink/netlink_sockif.c
+++ net/netlink/netlink_sockif.c
@@ -745,7 +745,6 @@ static ssize_t netlink_recvmsg(FAR struct socket *psock,
 static int netlink_close(FAR struct socket *psock)
 {
   FAR struct netlink_conn_s *conn = psock->s_conn;
-  int ret = OK;
 
   /* Perform some pre-close operations for the NETLINK socket type. */
 
@@ -759,14 +758,6 @@ static int netlink_close(FAR struct socket *psock)
 
       conn->crefs = 0;
       netlink_free(psock->s_conn);
-
-      if (ret < 0)
-        {
-          /* Return with error code, but free resources. */
-
-          nerr("ERROR: netlink_close failed: %d\n", ret);
-          return ret;
-        }
     }
   else
     {
@@ -775,7 +766,7 @@ static int netlink_close(FAR struct socket *psock)
       conn->crefs--;
     }
 
-  return ret;
+  return OK;
 }
 
 #endif /* CONFIG_NET_NETLINK */
