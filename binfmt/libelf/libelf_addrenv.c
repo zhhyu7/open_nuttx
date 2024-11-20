apache/nuttx

binfmt/elf: Fix the minor style issue
and remove the unused macros and unnecessary cast

Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

binfmt/libelf/libelf_addrenv.c
@@ -40,7 +40,7 @@
  ****************************************************************************/
 
 #define ELF_TEXT_WRE (PROT_READ | PROT_WRITE | PROT_EXEC)
-#define ELF_TEXT_WRD (PROT_READ | PROT_EXEC)
+#define ELF_TEXT_RE  (PROT_READ | PROT_EXEC)
 
 /****************************************************************************
  * Private Constant Data
@@ -144,14 +144,13 @@ errout_with_addrenv:
 
   /* Allocate memory to hold the ELF image */
 
-#if defined(CONFIG_ARCH_USE_TEXT_HEAP)
+#  if defined(CONFIG_ARCH_USE_TEXT_HEAP)
   loadinfo->textalloc = (uintptr_t)
-                         up_textheap_memalign(loadinfo->textalign,
-                                              textsize);
-#else
-  loadinfo->textalloc = (uintptr_t)kumm_memalign(loadinfo->textalign,
-                                                 textsize);
-#endif
+                        up_textheap_memalign(loadinfo->textalign, textsize);
+#  else
+  loadinfo->textalloc = (uintptr_t)
+                        kumm_memalign(loadinfo->textalign, textsize);
+#  endif
 
   if (!loadinfo->textalloc)
     {
@@ -160,8 +159,8 @@ errout_with_addrenv:
 
   if (loadinfo->datasize > 0)
     {
-      loadinfo->dataalloc = (uintptr_t)kumm_memalign(loadinfo->dataalign,
-                                                     datasize);
+      loadinfo->dataalloc = (uintptr_t)
+                            kumm_memalign(loadinfo->dataalign, datasize);
       if (!loadinfo->dataalloc)
         {
           return -ENOMEM;
@@ -236,7 +235,7 @@ int elf_addrenv_restore(FAR struct elf_loadinfo_s *loadinfo)
   /* Remove write access to .text */
 
   ret = up_addrenv_mprot(&loadinfo->addrenv->addrenv, loadinfo->textalloc,
-                         loadinfo->textsize, ELF_TEXT_WRD);
+                         loadinfo->textsize, ELF_TEXT_RE);
   if (ret < 0)
     {
       berr("ERROR: up_addrenv_text_disable_write failed: %d\n", ret);
@@ -261,7 +260,7 @@ int elf_addrenv_restore(FAR struct elf_loadinfo_s *loadinfo)
  *
  * Description:
  *   Release the address environment previously created by
- *   elf_addrenv_alloc().  This function  is called only under certain error
+ *   elf_addrenv_alloc().  This function is called only under certain error
  *   conditions after the module has been loaded but not yet started.
  *   After the module has been started, the address environment will
  *   automatically be freed when the module exits.
@@ -285,18 +284,17 @@ void elf_addrenv_free(FAR struct elf_loadinfo_s *loadinfo)
 
   if (loadinfo->textalloc != 0)
     {
-#if defined(CONFIG_ARCH_USE_TEXT_HEAP)
+#  if defined(CONFIG_ARCH_USE_TEXT_HEAP)
       up_textheap_free((FAR void *)loadinfo->textalloc);
-#else
+#  else
       kumm_free((FAR void *)loadinfo->textalloc);
-#endif
+#  endif
     }
 
   if (loadinfo->dataalloc != 0)
     {
       kumm_free((FAR void *)loadinfo->dataalloc);
     }
-
 #endif
 
   /* Clear out all indications of the allocated address environment */

zhhyu7/open_nuttx

Change-Id: Id8e6df7c74e8adb8cecafc99fb96faeb5068f7d4

binfmt/libelf/libelf_addrenv.c
@@ -40,7 +40,7 @@
  ****************************************************************************/
 
 #define ELF_TEXT_WRE (PROT_READ | PROT_WRITE | PROT_EXEC)
-#define ELF_TEXT_WRD (PROT_READ | PROT_EXEC)
+#define ELF_TEXT_RE  (PROT_READ | PROT_EXEC)
 
 /****************************************************************************
  * Private Constant Data
@@ -133,14 +133,13 @@ int elf_addrenv_alloc(FAR struct elf_loadinfo_s *loadinfo, size_t textsize,
 
   /* Allocate memory to hold the ELF image */
 
-#if defined(CONFIG_ARCH_USE_TEXT_HEAP)
+#  if defined(CONFIG_ARCH_USE_TEXT_HEAP)
   loadinfo->textalloc = (uintptr_t)
-                         up_textheap_memalign(loadinfo->textalign,
-                                              textsize);
-#else
-  loadinfo->textalloc = (uintptr_t)kumm_memalign(loadinfo->textalign,
-                                                 textsize);
-#endif
+                        up_textheap_memalign(loadinfo->textalign, textsize);
+#  else
+  loadinfo->textalloc = (uintptr_t)
+                        kumm_memalign(loadinfo->textalign, textsize);
+#  endif
 
   if (!loadinfo->textalloc)
     {
@@ -149,8 +148,8 @@ int elf_addrenv_alloc(FAR struct elf_loadinfo_s *loadinfo, size_t textsize,
 
   if (loadinfo->datasize > 0)
     {
-      loadinfo->dataalloc = (uintptr_t)kumm_memalign(loadinfo->dataalign,
-                                                     datasize);
+      loadinfo->dataalloc = (uintptr_t)
+                            kumm_memalign(loadinfo->dataalign, datasize);
       if (!loadinfo->dataalloc)
         {
           return -ENOMEM;
@@ -225,7 +224,7 @@ int elf_addrenv_restore(FAR struct elf_loadinfo_s *loadinfo)
   /* Remove write access to .text */
 
   ret = up_addrenv_mprot(&loadinfo->addrenv.addrenv, loadinfo->textalloc,
-                         loadinfo->textsize, ELF_TEXT_WRD);
+                         loadinfo->textsize, ELF_TEXT_WR);
   if (ret < 0)
     {
       berr("ERROR: up_addrenv_text_disable_write failed: %d\n", ret);
@@ -250,7 +249,7 @@ int elf_addrenv_restore(FAR struct elf_loadinfo_s *loadinfo)
  *
  * Description:
  *   Release the address environment previously created by
- *   elf_addrenv_alloc().  This function  is called only under certain error
+ *   elf_addrenv_alloc().  This function is called only under certain error
  *   conditions after the module has been loaded but not yet started.
  *   After the module has been started, the address environment will
  *   automatically be freed when the module exits.
@@ -279,18 +278,17 @@ void elf_addrenv_free(FAR struct elf_loadinfo_s *loadinfo)
 
   if (loadinfo->textalloc != 0)
     {
-#if defined(CONFIG_ARCH_USE_TEXT_HEAP)
+#  if defined(CONFIG_ARCH_USE_TEXT_HEAP)
       up_textheap_free((FAR void *)loadinfo->textalloc);
-#else
+#  else
       kumm_free((FAR void *)loadinfo->textalloc);
-#endif
+#  endif
     }
 
   if (loadinfo->dataalloc != 0)
     {
       kumm_free((FAR void *)loadinfo->dataalloc);
     }
-
 #endif
 
   /* Clear out all indications of the allocated address environment */