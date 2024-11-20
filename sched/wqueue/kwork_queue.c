hujun260/nuttx

sched/wqueue: Do as much work as possible in work_thread
MIRTOS4BES-246

Decouple the semcount and the work queue length.

Previous Problem:

If a work is queued and cancelled in high priority threads (or queued
by timer and cancelled by another high priority thread) before
work_thread runs, the queue operation will mark work_thread as ready to
run, but the cancel operation minus the semcount back to -1 and makes
wqueue->q empty. Then the work_thread still runs, found empty queue,
and wait sem again, then semcount becomes -2 (being minused by 1)

This can be done multiple times, then semcount can become very small
value. Test case to produce incorrect semcount:

high_priority_task()
{
  for (int i = 0; i < 10000; i++)
    {
      work_queue(LPWORK, &work, worker, NULL, 0);
      work_cancel(LPWORK, &work);
      usleep(1);
    }

  /* Now the g_lpwork.sem.semcount is a value near -10000 */
}

With incorrect semcount, any queue operation when the work_thread is
busy, will only increase semcount and push work into queue, but cannot
trigger work_thread (semcount is negative but work_thread is not
waiting), then there will be more and more works left in queue while
the work_thread is waiting sem and cannot call them.

Change-Id: If1aa68fedb435cca209f2aa89683db3ea43cdaea
Signed-off-by: Zhe Weng <wengzhe@xiaomi.com>

sched/wqueue/kwork_queue.c
@@ -39,6 +39,23 @@
 #ifdef CONFIG_SCHED_WORKQUEUE
 
 /****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define queue_work(wqueue, work) \
+  do \
+    { \
+      int sem_count; \
+      dq_addlast((FAR dq_entry_t *)(work), &(wqueue).q); \
+      sem_getvalue(&(wqueue).sem, &sem_count); \
+      if (sem_count < 0) /* There are threads waiting for sem. */ \
+        { \
+          nxsem_post(&(wqueue).sem); \
+        } \
+    } \
+  while (0)
+
+/****************************************************************************
  * Private Functions
  ****************************************************************************/
 
@@ -50,8 +67,7 @@
 static void hp_work_timer_expiry(wdparm_t arg)
 {
   irqstate_t flags = enter_critical_section();
-  dq_addlast((FAR dq_entry_t *)arg, &g_hpwork.q);
-  nxsem_post(&g_hpwork.sem);
+  queue_work(g_hpwork, arg);
   leave_critical_section(flags);
 }
 #endif
@@ -64,8 +80,7 @@ static void hp_work_timer_expiry(wdparm_t arg)
 static void lp_work_timer_expiry(wdparm_t arg)
 {
   irqstate_t flags = enter_critical_section();
-  dq_addlast((FAR dq_entry_t *)arg, &g_lpwork.q);
-  nxsem_post(&g_lpwork.sem);
+  queue_work(g_lpwork, arg);
   leave_critical_section(flags);
 }
 #endif
@@ -137,8 +152,7 @@ int work_queue(int qid, FAR struct work_s *work, worker_t worker,
 
       if (!delay)
         {
-          dq_addlast((FAR dq_entry_t *)work, &g_hpwork.q);
-          nxsem_post(&g_hpwork.sem);
+          queue_work(g_hpwork, work);
         }
       else
         {
@@ -155,8 +169,7 @@ int work_queue(int qid, FAR struct work_s *work, worker_t worker,
 
       if (!delay)
         {
-          dq_addlast((FAR dq_entry_t *)work, &g_lpwork.q);
-          nxsem_post(&g_lpwork.sem);
+          queue_work(g_lpwork, work);
         }
       else
         {

zhhyu7/open_nuttx

Change-Id: I53a270e233c74d6f80cb36777484f86af3458fbf

sched/wqueue/kwork_queue.c
@@ -39,6 +39,23 @@
 #ifdef CONFIG_SCHED_WORKQUEUE
 
 /****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define queue_work(wqueue, work) \
+  do \
+    { \
+      int sem_count; \
+      dq_addlast((FAR dq_entry_t *)(work), &(wqueue).q); \
+      nxsem_get_value(&(wqueue).sem, &sem_count); \
+      if (sem_count < 0) /* There are threads waiting for sem. */ \
+        { \
+          nxsem_post(&(wqueue).sem); \
+        } \
+    } \
+  while (0)
+
+/****************************************************************************
  * Private Functions
  ****************************************************************************/
 
@@ -50,8 +67,7 @@
 static void hp_work_timer_expiry(wdparm_t arg)
 {
   irqstate_t flags = enter_critical_section();
-  dq_addlast((FAR dq_entry_t *)arg, &g_hpwork.q);
-  nxsem_post(&g_hpwork.sem);
+  queue_work(g_hpwork, arg);
   leave_critical_section(flags);
 }
 #endif
@@ -64,8 +80,7 @@ static void hp_work_timer_expiry(wdparm_t arg)
 static void lp_work_timer_expiry(wdparm_t arg)
 {
   irqstate_t flags = enter_critical_section();
-  dq_addlast((FAR dq_entry_t *)arg, &g_lpwork.q);
-  nxsem_post(&g_lpwork.sem);
+  queue_work(g_lpwork, arg);
   leave_critical_section(flags);
 }
 #endif
@@ -137,8 +152,7 @@ int work_queue(int qid, FAR struct work_s *work, worker_t worker,
 
       if (!delay)
         {
-          dq_addlast((FAR dq_entry_t *)work, &g_hpwork.q);
-          nxsem_post(&g_hpwork.sem);
+          queue_work(g_hpwork, work);
         }
       else
         {
@@ -155,8 +169,7 @@ int work_queue(int qid, FAR struct work_s *work, worker_t worker,
 
       if (!delay)
         {
-          dq_addlast((FAR dq_entry_t *)work, &g_lpwork.q);
-          nxsem_post(&g_lpwork.sem);
+          queue_work(g_lpwork, work);
         }
       else
         {