apache/nuttx

irq: dynaminc create g_irqmap
reason:
dynaminc create g_irqmap to reduce the use of data segments
CONFIG_ARCH_NUSER_INTERRUPTS should be one more than the number of IRQs actually used

Signed-off-by: hujun5 <hujun5@xiaomi.com>

sched/irq/irq.h
@@ -44,7 +44,9 @@
 #  error CONFIG_ARCH_NUSER_INTERRUPTS is not defined
 #endif
 
-#ifdef CONFIG_ARCH_MINIMAL_VECTORTABLE
+#if defined(CONFIG_ARCH_MINIMAL_VECTORTABLE_DYNAMIC)
+#  define IRQ_TO_NDX(irq) (g_irqmap[irq] ? g_irqmap[irq] : irq_to_ndx(irq))
+#elif defined(CONFIG_ARCH_MINIMAL_VECTORTABLE)
 #  define IRQ_TO_NDX(irq) \
   (g_irqmap[(irq)] < CONFIG_ARCH_NUSER_INTERRUPTS ? g_irqmap[(irq)] : -EINVAL)
 #else
@@ -94,7 +96,6 @@ extern struct irq_info_s g_irqvector[CONFIG_ARCH_NUSER_INTERRUPTS];
 extern struct irq_info_s g_irqvector[NR_IRQS];
 #endif
 
-#ifdef CONFIG_ARCH_MINIMAL_VECTORTABLE
 /* This is the interrupt vector mapping table.  This must be provided by
  * architecture specific logic if CONFIG_ARCH_MINIMAL_VECTORTABLE is define
  * in the configuration.
@@ -104,6 +105,10 @@ extern struct irq_info_s g_irqvector[NR_IRQS];
  * declaration is here for the time being.
  */
 
+#if defined(CONFIG_ARCH_MINIMAL_VECTORTABLE_DYNAMIC)
+extern irq_mapped_t g_irqmap[NR_IRQS];
+int irq_to_ndx(int irq);
+#elif defined(CONFIG_ARCH_MINIMAL_VECTORTABLE)
 extern const irq_mapped_t g_irqmap[NR_IRQS];
 #endif
 

sched/irq/irq_attach.c
@@ -31,8 +31,50 @@
 #include "irq/irq.h"
 
 /****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+#ifdef CONFIG_ARCH_MINIMAL_VECTORTABLE_DYNAMIC
+static int g_irqmap_count = 1;
+#endif
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+#ifdef CONFIG_ARCH_MINIMAL_VECTORTABLE_DYNAMIC
+
+/* This is the interrupt vector mapping table.  This must be provided by
+ * architecture specific logic if CONFIG_ARCH_MINIMAL_VECTORTABLE is define
+ * in the configuration.
+ *
+ * REVISIT: This should be declared in include/nuttx/irq.h.  The declaration
+ * at that location, however, introduces a circular include dependency so the
+ * declaration is here for the time being.
+ */
+
+irq_mapped_t g_irqmap[NR_IRQS];
+#endif
+
+/****************************************************************************
  * Public Functions
  ****************************************************************************/
+
+#ifdef CONFIG_ARCH_MINIMAL_VECTORTABLE_DYNAMIC
+int irq_to_ndx(int irq)
+{
+  DEBUGASSERT(g_irqmap_count < CONFIG_ARCH_NUSER_INTERRUPTS);
+
+  irqstate_t flags = spin_lock_irqsave(NULL);
+  if (g_irqmap[irq] == 0)
+    {
+      g_irqmap[irq] = g_irqmap_count++;
+    }
+
+  spin_unlock_irqrestore(NULL, flags);
+  return g_irqmap[irq];
+}
+#endif
 
 /****************************************************************************
  * Name: irq_attach

zhhyu7/open_nuttx

Change-Id: I9b8f4c7976043bfb5c1938e56f8bedda32bb9ea7

sched/irq/irq.h
@@ -44,7 +44,9 @@
 #  error CONFIG_ARCH_NUSER_INTERRUPTS is not defined
 #endif
 
-#ifdef CONFIG_ARCH_MINIMAL_VECTORTABLE
+#if defined(CONFIG_ARCH_MINIMAL_VECTORTABLE_DYNAMINC)
+#  define IRQ_TO_NDX(irq) (g_irqmap[irq] ? g_irqmap[irq] : irq_to_ndx(irq))
+#elif defined(CONFIG_ARCH_MINIMAL_VECTORTABLE)
 #  define IRQ_TO_NDX(irq) \
   (g_irqmap[(irq)] < CONFIG_ARCH_NUSER_INTERRUPTS ? g_irqmap[(irq)] : -EINVAL)
 #else
@@ -94,7 +96,6 @@ extern struct irq_info_s g_irqvector[CONFIG_ARCH_NUSER_INTERRUPTS];
 extern struct irq_info_s g_irqvector[NR_IRQS];
 #endif
 
-#ifdef CONFIG_ARCH_MINIMAL_VECTORTABLE
 /* This is the interrupt vector mapping table.  This must be provided by
  * architecture specific logic if CONFIG_ARCH_MINIMAL_VECTORTABLE is define
  * in the configuration.
@@ -104,6 +105,10 @@ extern struct irq_info_s g_irqvector[NR_IRQS];
  * declaration is here for the time being.
  */
 
+#if defined(CONFIG_ARCH_MINIMAL_VECTORTABLE_DYNAMINC)
+extern irq_mapped_t g_irqmap[NR_IRQS];
+int irq_to_ndx(int irq);
+#elif defined(CONFIG_ARCH_MINIMAL_VECTORTABLE)
 extern const irq_mapped_t g_irqmap[NR_IRQS];
 #endif
 

sched/irq/irq_attach.c
@@ -31,8 +31,50 @@
 #include "irq/irq.h"
 
 /****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+#ifdef CONFIG_ARCH_MINIMAL_VECTORTABLE_DYNAMINC
+static int g_irqmap_count = 1;
+#endif
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+#ifdef CONFIG_ARCH_MINIMAL_VECTORTABLE_DYNAMINC
+
+/* This is the interrupt vector mapping table.  This must be provided by
+ * architecture specific logic if CONFIG_ARCH_MINIMAL_VECTORTABLE is define
+ * in the configuration.
+ *
+ * REVISIT: This should be declared in include/nuttx/irq.h.  The declaration
+ * at that location, however, introduces a circular include dependency so the
+ * declaration is here for the time being.
+ */
+
+irq_mapped_t g_irqmap[NR_IRQS];
+#endif
+
+/****************************************************************************
  * Public Functions
  ****************************************************************************/
+
+#ifdef CONFIG_ARCH_MINIMAL_VECTORTABLE_DYNAMINC
+int irq_to_ndx(int irq)
+{
+  DEBUGASSERT(g_irqmap_count < CONFIG_ARCH_NUSER_INTERRUPTS);
+
+  irqstate_t flags = spin_lock_irqsave(NULL);
+  if (g_irqmap[irq] == 0)
+    {
+      g_irqmap[irq] = g_irqmap_count++;
+    }
+
+  spin_unlock_irqrestore(NULL, flags);
+  return g_irqmap[irq];
+}
+#endif
 
 /****************************************************************************
  * Name: irq_attach