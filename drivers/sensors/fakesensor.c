apache/nuttx

driver/sensor: add struct file for all sensor_ops_s
VELAPLATFO-1522

Change-Id: If9fdbe829ce20eaaeb0d333ba8fe924f6a19aa21
Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

drivers/sensors/fakesensor.c
@@ -59,11 +59,13 @@ struct fakesensor_s
  * Private Function Prototypes
  ****************************************************************************/
 
-static int fakesensor_activate(FAR struct sensor_lowerhalf_s *lower,
-                               bool sw);
-static int fakesensor_set_interval(FAR struct sensor_lowerhalf_s *lower,
+static int fakesensor_activate(FAR struct file *filep,
+                               FAR struct sensor_lowerhalf_s *lower, bool sw);
+static int fakesensor_set_interval(FAR struct file *filep,
+                                   FAR struct sensor_lowerhalf_s *lower,
                                    FAR unsigned long *period_us);
-static int fakesensor_batch(FAR struct sensor_lowerhalf_s *lower,
+static int fakesensor_batch(FAR struct file *filep,
+                            FAR struct sensor_lowerhalf_s *lower,
                             FAR unsigned long *latency_us);
 static void fakesensor_push_event(FAR struct sensor_lowerhalf_s *lower);
 static int fakesensor_thread(int argc, char** argv);
@@ -213,7 +215,8 @@ static inline void fakesensor_read_gps(FAR struct fakesensor_s *sensor)
                            sizeof(struct sensor_gps));
 }
 
-static int fakesensor_activate(FAR struct sensor_lowerhalf_s *lower, bool sw)
+static int fakesensor_activate(FAR struct file *filep,
+                               FAR struct sensor_lowerhalf_s *lower, bool sw)
 {
   FAR struct fakesensor_s *sensor = container_of(lower,
                                                  struct fakesensor_s, lower);
@@ -233,7 +236,8 @@ static int fakesensor_activate(FAR struct sensor_lowerhalf_s *lower, bool sw)
   return OK;
 }
 
-static int fakesensor_set_interval(FAR struct sensor_lowerhalf_s *lower,
+static int fakesensor_set_interval(FAR struct file *filep,
+                                   FAR struct sensor_lowerhalf_s *lower,
                                    FAR unsigned long *period_us)
 {
   FAR struct fakesensor_s *sensor = container_of(lower,
@@ -242,7 +246,8 @@ static int fakesensor_set_interval(FAR struct sensor_lowerhalf_s *lower,
   return OK;
 }
 
-static int fakesensor_batch(FAR struct sensor_lowerhalf_s *lower,
+static int fakesensor_batch(FAR struct file *filep,
+                            FAR struct sensor_lowerhalf_s *lower,
                             FAR unsigned long *latency_us)
 {
   FAR struct fakesensor_s *sensor = container_of(lower,

drivers/sensors/wtgahrs2.c
@@ -95,8 +95,10 @@ struct wtgahrs2_dev_s
  * Private Function Prototypes
  ****************************************************************************/
 
-static int wtgahrs2_activate(FAR struct sensor_lowerhalf_s *lower, bool sw);
-static int wtgahrs2_set_interval(FAR struct sensor_lowerhalf_s *lower,
+static int wtgahrs2_activate(FAR struct file *filep,
+                             FAR struct sensor_lowerhalf_s *lower, bool sw);
+static int wtgahrs2_set_interval(FAR struct file *filep,
+                                 FAR struct sensor_lowerhalf_s *lower,
                                  FAR unsigned long *interval);
 
 /****************************************************************************
@@ -152,7 +154,8 @@ static void wtgahrs2_sendcmd(FAR struct wtgahrs2_dev_s *rtdata,
   usleep(10000);
 }
 
-static int wtgahrs2_activate(FAR struct sensor_lowerhalf_s *lower, bool sw)
+static int wtgahrs2_activate(FAR struct file *filep,
+                             FAR struct sensor_lowerhalf_s *lower, bool sw)
 {
   FAR struct wtgahrs2_sensor_s *dev = (FAR struct wtgahrs2_sensor_s *)lower;
   dev->enable = sw;
@@ -160,7 +163,8 @@ static int wtgahrs2_activate(FAR struct sensor_lowerhalf_s *lower, bool sw)
   return 0;
 }
 
-static int wtgahrs2_set_interval(FAR struct sensor_lowerhalf_s *lower,
+static int wtgahrs2_set_interval(FAR struct file *filep,
+                                 FAR struct sensor_lowerhalf_s *lower,
                                  FAR unsigned long *interval)
 {
   FAR struct wtgahrs2_sensor_s *dev = (FAR struct wtgahrs2_sensor_s *)lower;

zhhyu7/open_nuttx

Change-Id: I2bbea0c04ab0a4d12eb938b8612e049b7559b14c

drivers/sensors/fakesensor.c
@@ -60,10 +60,12 @@ struct fakesensor_s
  ****************************************************************************/
 
 static int fakesensor_activate(FAR struct sensor_lowerhalf_s *lower,
-                               bool sw);
+                               FAR struct file *filep, bool sw);
 static int fakesensor_set_interval(FAR struct sensor_lowerhalf_s *lower,
+                                   FAR struct file *filep,
                                    FAR unsigned long *period_us);
 static int fakesensor_batch(FAR struct sensor_lowerhalf_s *lower,
+                            FAR struct file *filep,
                             FAR unsigned long *latency_us);
 static void fakesensor_push_event(FAR struct sensor_lowerhalf_s *lower);
 static int fakesensor_thread(int argc, char** argv);
@@ -213,7 +215,8 @@ static inline void fakesensor_read_gps(FAR struct fakesensor_s *sensor)
                            sizeof(struct sensor_gps));
 }
 
-static int fakesensor_activate(FAR struct sensor_lowerhalf_s *lower, bool sw)
+static int fakesensor_activate(FAR struct sensor_lowerhalf_s *lower,
+                               FAR struct file *filep, bool sw)
 {
   FAR struct fakesensor_s *sensor = container_of(lower,
                                                  struct fakesensor_s, lower);
@@ -234,6 +237,7 @@ static int fakesensor_activate(FAR struct sensor_lowerhalf_s *lower, bool sw)
 }
 
 static int fakesensor_set_interval(FAR struct sensor_lowerhalf_s *lower,
+                                   FAR struct file *filep,
                                    FAR unsigned long *period_us)
 {
   FAR struct fakesensor_s *sensor = container_of(lower,
@@ -243,6 +247,7 @@ static int fakesensor_set_interval(FAR struct sensor_lowerhalf_s *lower,
 }
 
 static int fakesensor_batch(FAR struct sensor_lowerhalf_s *lower,
+                            FAR struct file *filep,
                             FAR unsigned long *latency_us)
 {
   FAR struct fakesensor_s *sensor = container_of(lower,

drivers/sensors/wtgahrs2.c
@@ -95,8 +95,10 @@ struct wtgahrs2_dev_s
  * Private Function Prototypes
  ****************************************************************************/
 
-static int wtgahrs2_activate(FAR struct sensor_lowerhalf_s *lower, bool sw);
+static int wtgahrs2_activate(FAR struct sensor_lowerhalf_s *lower,
+                             FAR struct file *filep, bool sw);
 static int wtgahrs2_set_interval(FAR struct sensor_lowerhalf_s *lower,
+                                 FAR struct file *filep,
                                  FAR unsigned long *interval);
 
 /****************************************************************************
@@ -152,7 +154,8 @@ static void wtgahrs2_sendcmd(FAR struct wtgahrs2_dev_s *rtdata,
   usleep(10000);
 }
 
-static int wtgahrs2_activate(FAR struct sensor_lowerhalf_s *lower, bool sw)
+static int wtgahrs2_activate(FAR struct sensor_lowerhalf_s *lower,
+                             FAR struct file *filep, bool sw)
 {
   FAR struct wtgahrs2_sensor_s *dev = (FAR struct wtgahrs2_sensor_s *)lower;
   dev->enable = sw;
@@ -161,6 +164,7 @@ static int wtgahrs2_activate(FAR struct sensor_lowerhalf_s *lower, bool sw)
 }
 
 static int wtgahrs2_set_interval(FAR struct sensor_lowerhalf_s *lower,
+                                 FAR struct file *filep,
                                  FAR unsigned long *interval)
 {
   FAR struct wtgahrs2_sensor_s *dev = (FAR struct wtgahrs2_sensor_s *)lower;