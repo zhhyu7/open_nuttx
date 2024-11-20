apache/nuttx

libc/gettext: Initialize path field when creating mofile struct.
Signed-off-by: Xu Xingliang <xuxingliang@xiaomi.com>
Change-Id: Ic88876b254e29ac6156f8b2b15c5321af62f4e24

libs/libc/locale/lib_gettext.c
@@ -312,6 +312,7 @@ FAR char *dcngettext(FAR const char *domainname,
           return notrans;
         }
 
+      strncpy(mofile->path, path, PATH_MAX);
       mofile->map = momap(path, &mofile->size);
       if (mofile->map == MAP_FAILED)
         {

zhhyu7/open_nuttx

Change-Id: I7c7f1a2ef4e05e0846849e8bcc7f3fb3c8fcbf43

libs/libc/locale/lib_gettext.c
@@ -312,6 +312,7 @@ FAR char *dcngettext(FAR const char *domainname,
           return notrans;
         }
 
+      strlcpy(mofile->path, path, PATH_MAX);
       mofile->map = momap(path, &mofile->size);
       if (mofile->map == MAP_FAILED)
         {