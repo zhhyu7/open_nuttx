apache/nuttx

fs/procfs: Supports any number of thread displays
After the number of threads exceeds the array size, it will not be displayed.
Any number of threads can be displayed using dynamic adaptation

Signed-off-by: yinshengkai <yinshengkai@xiaomi.com>

fs/procfs/fs_procfs.c
@@ -47,6 +47,7 @@
 #include <nuttx/fs/procfs.h>
 
 #include "mount/mount.h"
+#include "sched/sched.h"
 
 /****************************************************************************
  * External Definitions
@@ -315,8 +316,8 @@ struct procfs_level0_s
   /* Our private data */
 
   uint8_t lastlen;                       /* length of last reported static dir */
-  pid_t pid[CONFIG_FS_PROCFS_MAX_TASKS]; /* Snapshot of all active task IDs */
   FAR const char *lastread;              /* Pointer to last static dir read */
+  pid_t pid[1];                          /* Snapshot of all active task IDs */
 };
 
 /* Level 1 is an internal virtual directory (such as /proc/fs) which
@@ -355,14 +356,14 @@ static void procfs_enum(FAR struct tcb_s *tcb, FAR void *arg)
 
   /* Add the PID to the list */
 
-  index = dir->base.nentries;
-  if (index >= CONFIG_FS_PROCFS_MAX_TASKS)
+  if (dir->base.index >= dir->base.nentries)
     {
       return;
     }
 
+  index = dir->base.index;
   dir->pid[index] = tcb->pid;
-  dir->base.nentries = index + 1;
+  dir->base.index = index + 1;
 }
 
 /****************************************************************************
@@ -633,12 +634,18 @@ static int procfs_opendir(FAR struct inode *mountpt, FAR const char *relpath,
 
   if (!relpath || relpath[0] == '\0')
     {
+      size_t num = 0;
+
       /* The path refers to the top level directory.  Allocate the level0
        * dirent structure.
        */
 
+#ifndef CONFIG_FS_PROCFS_EXCLUDE_PROCESS
+      num = g_npidhash;
+#endif
+
       level0 = (FAR struct procfs_level0_s *)
-         kmm_zalloc(sizeof(struct procfs_level0_s));
+         kmm_zalloc(sizeof(struct procfs_level0_s) + sizeof(pid_t) * num) ;
       if (!level0)
         {
           ferr("ERROR: Failed to allocate the level0 directory structure\n");
@@ -653,7 +660,11 @@ static int procfs_opendir(FAR struct inode *mountpt, FAR const char *relpath,
        */
 
 #ifndef CONFIG_FS_PROCFS_EXCLUDE_PROCESS
+      level0->base.index = 0;
+      level0->base.nentries = num;
       nxsched_foreach(procfs_enum, level0);
+      level0->base.nentries = level0->base.index;
+      level0->base.index = 0;
       procfs_sort_pid(level0);
 #else
       level0->base.index = 0;

zhhyu7/open_nuttx

Change-Id: I5f6d343f102aa3fd2565b2b38fbb4b5671e12847

fs/procfs/fs_procfs.c
@@ -47,6 +47,7 @@
 #include <nuttx/fs/procfs.h>
 
 #include "mount/mount.h"
+#include "sched/sched.h"
 #include "fs_heap.h"
 
 /****************************************************************************
@@ -328,7 +329,7 @@ struct procfs_level0_s
 
   uint8_t lastlen;                       /* length of last reported static dir */
   FAR const char *lastread;              /* Pointer to last static dir read */
-  pid_t pid[0];                          /* Snapshot of all active task IDs */
+  pid_t pid[1];                          /* Snapshot of all active task IDs */
 };
 
 /* Level 1 is an internal virtual directory (such as /proc/fs) which
@@ -375,15 +376,6 @@ static void procfs_enum(FAR struct tcb_s *tcb, FAR void *arg)
   index = dir->base.index;
   dir->pid[index] = tcb->pid;
   dir->base.index = index + 1;
-}
-
-/****************************************************************************
- * Name: procfs_thread_number
- ****************************************************************************/
-
-static void procfs_thread_number(FAR struct tcb_s *tcb, FAR void *arg)
-{
-  (*(FAR size_t *)arg)++;
 }
 
 /****************************************************************************
@@ -666,11 +658,11 @@ static int procfs_opendir(FAR struct inode *mountpt, FAR const char *relpath,
        */
 
 #ifndef CONFIG_FS_PROCFS_EXCLUDE_PROCESS
-      nxsched_foreach(procfs_thread_number, &num);
+      num = g_npidhash;
 #endif
 
       level0 = (FAR struct procfs_level0_s *)
-         fs_heap_zalloc(sizeof(struct procfs_level0_s) + sizeof(pid_t) * num) ;
+      fs_heap_zalloc(sizeof(struct procfs_level0_s) + sizeof(pid_t) * num);
       if (!level0)
         {
           ferr("ERROR: Failed to allocate the level0 directory structure\n");