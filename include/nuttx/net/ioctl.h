SPRESENSE/nuttx

include: nuttx: Re-allocate SIOCDENYINETSOCK
The SIOCDENYINETSOCK definition is duplicate with SIOCSIFMTU.
So, re-allocate SIOCDENYINETSOCK command number.

include/nuttx/net/ioctl.h
@@ -124,7 +124,7 @@
 
 /* Network socket control ***************************************************/
 
-#define SIOCDENYINETSOCK _SIOC(0x0033) /* Deny network socket. */
+#define SIOCDENYINETSOCK _SIOC(0x0036) /* Deny network socket. */
 
 /****************************************************************************
  * Public Type Definitions

zhhyu7/open_nuttx

include/nuttx/net/ioctl.h
@@ -125,7 +125,7 @@
 
 /* Network socket control ***************************************************/
 
-#define SIOCDENYINETSOCK _SIOC(0x0033) /* Deny network socket. */
+#define SIOCDENYINETSOCK _SIOC(0x003C) /* Deny network socket. */
 
 /* Bridge calls *************************************************************/
 