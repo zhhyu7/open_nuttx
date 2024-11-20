apache/nuttx

pthread: add tls exit flags when pthread_exit()
If receive SIGKILL during pthread_exit, pthread_exit may be
called twice, so add tls flags to prevent repeated calls.

Signed-off-by: ligd <liguiding1@xiaomi.com>

libs/libc/pthread/pthread_exit.c
@@ -55,6 +55,10 @@
 
 void pthread_exit(FAR void *exit_value)
 {
+  FAR struct tls_info_s *info = tls_get_info();
+
+  info->flags |= TLS_THREAD_EXIT_PROCESSING;
+
   /* Mark the pthread as non-cancelable to avoid additional calls to
    * pthread_exit() due to any cancellation point logic that might get
    * kicked off by actions taken during pthread_exit processing.

sched/signal/sig_default.c
@@ -37,6 +37,7 @@
 #include <nuttx/sched.h>
 #include <nuttx/spinlock.h>
 #include <nuttx/signal.h>
+#include <nuttx/tls.h>
 
 #include "group/group.h"
 #include "sched/sched.h"
@@ -230,12 +231,17 @@ static void nxsig_abnormal_termination(int signo)
 
   if ((rtcb->flags & TCB_FLAG_TTYPE_MASK) == TCB_FLAG_TTYPE_PTHREAD)
     {
+      FAR struct tls_info_s *info = tls_get_info();
+
       /* Exit the final thread of the task group.
        *
        * REVISIT:  This will not work if HAVE_GROUP_MEMBERS is not set.
        */
 
-      pthread_exit(NULL);
+      if ((info->flags & TLS_THREAD_EXIT_PROCESSING) == 0)
+        {
+          pthread_exit(NULL);
+        }
     }
   else
 #endif

zhhyu7/open_nuttx

Change-Id: I84d3bcf1f20e2740f6c8b7b462f91fa51d5f4553

libs/libc/pthread/pthread_exit.c
@@ -53,6 +53,10 @@
 
 void pthread_exit(FAR void *exit_value)
 {
+  FAR struct tls_info_s *info = tls_get_info();
+
+  info->flags |= TLS_THREAD_EIXT;
+
   /* Mark the pthread as non-cancelable to avoid additional calls to
    * pthread_exit() due to any cancellation point logic that might get
    * kicked off by actions taken during pthread_exit processing.

sched/signal/sig_default.c
@@ -35,6 +35,7 @@
 #include <nuttx/sched.h>
 #include <nuttx/spinlock.h>
 #include <nuttx/signal.h>
+#include <nuttx/tls.h>
 
 #include "group/group.h"
 #include "sched/sched.h"
@@ -228,12 +229,17 @@ static void nxsig_abnormal_termination(int signo)
 
   if ((rtcb->flags & TCB_FLAG_TTYPE_MASK) == TCB_FLAG_TTYPE_PTHREAD)
     {
+      FAR struct tls_info_s *info = tls_get_info();
+
       /* Exit the final thread of the task group.
        *
        * REVISIT:  This will not work if HAVE_GROUP_MEMBERS is not set.
        */
 
-      pthread_exit(NULL);
+      if ((info->flags & TLS_THREAD_EIXT) == 0)
+        {
+          pthread_exit(NULL);
+        }
     }
   else
 #endif