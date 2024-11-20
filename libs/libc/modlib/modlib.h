acassis/incubator-nuttx

Add support for the loading of ET_DYN objects
* build-globals.sh
  - Build the modlib_globals.S file used to resolve symbols when dynamically loading

* include/elf.h
  - Add definitions that may be found in shared objects

* include/nuttx/lib/modlib.h
  - Add fields to mod_loadifno_s:
    - Program headers
    - Exported symbols
    - Data section address
    - Padding requirement
    - Section index for dynamic symbol table
    - Number of symbols exported
  - Add prottotype for modlib_freesymtab

* libs/libc/dlfcn/lib_dlclose.c
  - Free the symbol table when the dll is closed

* libs/libc/dlfcn/lib_dlopen.c
  - Add dump of program headers to debug routine
  - Differentiate between ET_REL and ET_DYN objects

* libs/libc/machine/arm/armv7-m/arch_elf.c
  - Add handling of R_ARM_RELATIVE and R_ARM_JUMP slot relocation types

* libs/libc/modlib/Make.defs
  - Include new file containing library APIs used when loading shared objects

* libs/libc/modlib/modlib.h
  - Add parameter to modlib_readsym prototype
  - Add prototypes for:
    - modlib_insertsymtab
    - modlib_findglobal

* libs/libc/modlib/modlib_loadshdrs.c
  - Rename modlib_loadshdrs.c to modlib_loadhdrs.c
  - Rename modlib_loadshdrs to modlib_loadhdrs
  - Add code to load program headers

* libs/libc/modlib/modlib_bind.c
  - Call modlib_readsym with pointer to symbol table
  - Add modlib_relocatedyn to manage relocation of symbols with shared object (ET_DYN)
  - Differentiate between ET_REL and ET_DYN objects

* libs/libc/modlib/modlib_load.c
  - Calculate sizes and text/data addresses based on program headers rather than section headers

* libs/libc/modlib/modlib_symbols.c
  - Define entry point structure
  - Add offset parameter to modlib_symname() and use to find symbol names
  - Add symtab section header parameter to modlib_readsym()
  - Add offset parameter to modlib_symvalue() to locate symbol names
  - Add modlib_insertsyntab() to create a symbol table for exporting and resolution
  - Add findEP() to 

libs/libc/modlib/modlib.h
@@ -83,7 +83,7 @@ int modlib_findsymtab(FAR struct mod_loadinfo_s *loadinfo);
  ****************************************************************************/
 
 int modlib_readsym(FAR struct mod_loadinfo_s *loadinfo, int index,
-                   FAR Elf_Sym *sym);
+                   FAR Elf32_Sym *sym, FAR Elf32_Shdr *shdr);
 
 /****************************************************************************
  * Name: modlib_symvalue
@@ -110,13 +110,64 @@ int modlib_readsym(FAR struct mod_loadinfo_s *loadinfo, int index,
  ****************************************************************************/
 
 int modlib_symvalue(FAR struct module_s *modp,
-                    FAR struct mod_loadinfo_s *loadinfo, FAR Elf_Sym *sym);
+                    FAR struct mod_loadinfo_s *loadinfo, FAR Elf32_Sym *sym,
+ 		    Elf32_Off offset);
+
+ /****************************************************************************
+  * Name: modlib_insertsymtab
+  *
+  * Description:
+  *   Insert a symbol table for the current module.
+  *
+  * Input Parameters:
+  *   modp     - Module state information
+  *   loadinfo - Module load information
+  *   shdr     - Symbol table section header
+  *   sym      - Symbol table entry
+  *
+  * Returned Value:
+  *   0 (OK) is returned on success and a negated errno is returned on
+  *   failure.
+  *
+  *   ENOMEM - Symbol undefined and not provided via a symbol table
+  *
+  ****************************************************************************/
+ 
+int modlib_insertsymtab(FAR struct module_s *modp, 
+ 		        FAR struct mod_loadinfo_s *loadinfo, 
+                        FAR Elf32_Shdr *shdr,
+                        FAR Elf32_Sym *sym);
 
 /****************************************************************************
- * Name: modlib_loadshdrs
+ * Name: modlib_findglobal
  *
  * Description:
- *   Loads section headers into memory.
+ *   Find a symbol in the global symbol table
+ *
+ * Input Parameters:
+ *   modp     - Module state information
+ *   loadinfo - Module load information
+ *   shdr     - Symbol table section header
+ *   sym      - Symbol table entry
+ *
+ * Returned Value:
+ *   0 (OK) is returned on success and a negated errno is returned on
+ *   failure.
+ *
+ *   ENOMEM - Symbol undefined and not provided via a symbol table
+ *
+ ****************************************************************************/
+
+void *modlib_findglobal(FAR struct module_s *modp, 
+		        FAR struct mod_loadinfo_s *loadinfo, 
+                        FAR Elf32_Shdr *shdr,
+                        FAR Elf32_Sym *sym);
+
+/****************************************************************************
+ * Name: modlib_loadhdrs
+ *
+ * Description:
+ *   Loads program and section headers into memory.
  *
  * Returned Value:
  *   0 (OK) is returned on success and a negated errno is returned on
@@ -124,7 +175,7 @@ int modlib_symvalue(FAR struct module_s *modp,
  *
  ****************************************************************************/
 
-int modlib_loadshdrs(FAR struct mod_loadinfo_s *loadinfo);
+int modlib_loadhdrs(FAR struct mod_loadinfo_s *loadinfo);
 
 /****************************************************************************
  * Name: modlib_findsection

libs/libc/modlib/modlib_loadhdrs.c
@@ -42,10 +42,10 @@
  ****************************************************************************/
 
 /****************************************************************************
- * Name: modlib_loadshdrs
+ * Name: modlib_loadhdrs
  *
  * Description:
- *   Loads section headers into memory.
+ *   Loads program and section headers into memory.
  *
  * Returned Value:
  *   0 (OK) is returned on success and a negated errno is returned on
@@ -55,16 +55,25 @@
 
 int modlib_loadshdrs(FAR struct mod_loadinfo_s *loadinfo)
 {
-  size_t shdrsize;
+  size_t shdrsize, phdrsize;
   int ret;
 
   DEBUGASSERT(loadinfo->shdr == NULL);
+  DEBUGASSERT(loadinfo->phdr == NULL);
 
   /* Verify that there are sections */
 
   if (loadinfo->ehdr.e_shnum < 1)
     {
       berr("ERROR: No sections(?)\n");
+      return -EINVAL;
+    }
+
+  /* Verify that there are program headers */
+
+  if (loadinfo->ehdr.e_phnum < 1)
+    {
+      berr("ERROR: No program headers(?)\n");
       return -EINVAL;
     }
 
@@ -75,6 +84,15 @@ int modlib_loadshdrs(FAR struct mod_loadinfo_s *loadinfo)
   if (loadinfo->ehdr.e_shoff + shdrsize > loadinfo->filelen)
     {
       berr("ERROR: Insufficient space in file for section header table\n");
+      return -ESPIPE;
+    }
+
+  /* Get the total size of the program header table */
+
+  phdrsize = (size_t)loadinfo->ehdr.e_phentsize * (size_t)loadinfo->ehdr.e_phnum;
+  if (loadinfo->ehdr.e_phoff + phdrsize > loadinfo->filelen)
+    {
+      berr("ERROR: Insufficent space in file for program header table\n");
       return -ESPIPE;
     }
 
@@ -95,6 +113,28 @@ int modlib_loadshdrs(FAR struct mod_loadinfo_s *loadinfo)
   if (ret < 0)
     {
       berr("ERROR: Failed to read section header table: %d\n", ret);
+    }
+
+  /* Allocate memory to hold a working copy of the program header table */
+
+  loadinfo->phdr = (FAR FAR Elf32_Phdr *)lib_malloc(phdrsize);
+  if (!loadinfo->shdr)
+    {
+      lib_free((FAR void *)loadinfo->shdr);
+      berr("ERROR: Failed to allocate the program header table. Size: %ld\n",
+           (long)phdrsize);
+      return -ENOMEM;
+    }
+
+  /* Read the program header table into memory */
+
+  ret = modlib_read(loadinfo, (FAR uint8_t *)loadinfo->phdr, phdrsize,
+                    loadinfo->ehdr.e_phoff);
+  if (ret < 0)
+    {
+      berr("ERROR: Failed to read program header table: %d\n", ret);
+      lib_free((FAR void *)loadinfo->phdr);
+      lib_free((FAR void *)loadinfo->shdr);
     }
 
   return ret;

zhhyu7/open_nuttx

    * libs/libc/machine/arm/armv7-m/arch_elf.c
      - Fix cast of error message parameters
    * build-globals.sh
      - Build the modlib_globals.S file used to resolve symbols when dynamically loading
    * libs/libc/modlib/Make.defs
      - Build modlib_globals.S for all targets
    * libs/libc/modlib/modlib_load.c
      - Calculate sizes and text/data addresses based on program headers rather than section headers
      - Handle objects with no program headers
    * libs/libc/modlib/modlib_bind.c
      - Call modlib_readsym with pointer to symbol table
      - Add modlib_relocatedyn to manage relocation of symbols with shared object (ET_DYN)
      - Differentiate between ET_REL and ET_DYN objects
      - Use arch independent symbol ELF_R_SYM
      - Cast sizes to avoid warnings
    * include/elf.h
      - Add definitions that may be found in shared objects
    * include/nuttx/lib/modlib.h
      - Add parameter to modlib_readsym prototype
      - Add prototypes for:
        - modlib_insertsymtab
        - modlib_findglobal
      - Define Elf_Dyn and Elf_Off according to the elf architecture
      - Add fields to mod_loadifno_s:
        - Program headers
        - Exported symbols
        - Data section address
        - Padding requirement
        - Section index for dynamic symbol table
        - Number of symbols exported
      - Add prottotype for modlib_freesymtab
    * libs/libc/dlfcn/lib_dlclose.c
      - Free the symbol table when the dll is closed
    * libs/libc/dlfcn/lib_dlopen.c
      - Add dump of program headers to debug routine
      - Add handling of R_ARM_RELATIVE and R_ARM_JUMP slot relocation types
    * libs/libc/modlib/modlib_loadshdrs.c
      - Rename modlib_loadshdrs.c to modlib_loadhdrs.c
      - Rename modlib_loadshdrs to modlib_loadhdrs
      - Add code to load program headers
    * libs/libc/modlib/modlib_symbols.c
      - Define entry point structure
      - Add offset parameter to modlib_symname() and use to find symbol names
      - A

libs/libc/modlib/modlib.h
@@ -111,30 +111,30 @@ int modlib_readsym(FAR struct mod_loadinfo_s *loadinfo, int index,
 
 int modlib_symvalue(FAR struct module_s *modp,
                     FAR struct mod_loadinfo_s *loadinfo, FAR Elf_Sym *sym,
-                    Elf_Off offset);
+ 		    Elf_Off offset);
 
-/****************************************************************************
- * Name: modlib_insertsymtab
- *
- * Description:
- *   Insert a symbol table for the current module.
- *
- * Input Parameters:
- *   modp     - Module state information
- *   loadinfo - Module load information
- *   shdr     - Symbol table section header
- *   sym      - Symbol table entry
- *
- * Returned Value:
- *   0 (OK) is returned on success and a negated errno is returned on
- *   failure.
- *
- *   ENOMEM - Symbol undefined and not provided via a symbol table
- *
- ****************************************************************************/
-
-int modlib_insertsymtab(FAR struct module_s *modp,
-                        FAR struct mod_loadinfo_s *loadinfo,
+ /****************************************************************************
+  * Name: modlib_insertsymtab
+  *
+  * Description:
+  *   Insert a symbol table for the current module.
+  *
+  * Input Parameters:
+  *   modp     - Module state information
+  *   loadinfo - Module load information
+  *   shdr     - Symbol table section header
+  *   sym      - Symbol table entry
+  *
+  * Returned Value:
+  *   0 (OK) is returned on success and a negated errno is returned on
+  *   failure.
+  *
+  *   ENOMEM - Symbol undefined and not provided via a symbol table
+  *
+  ****************************************************************************/
+ 
+int modlib_insertsymtab(FAR struct module_s *modp, 
+ 		        FAR struct mod_loadinfo_s *loadinfo, 
                         FAR Elf_Shdr *shdr,
                         FAR Elf_Sym *sym);
 
@@ -158,8 +158,8 @@ int modlib_insertsymtab(FAR struct module_s *modp,
  *
  ****************************************************************************/
 
-void *modlib_findglobal(FAR struct module_s *modp,
-                        FAR struct mod_loadinfo_s *loadinfo,
+void *modlib_findglobal(FAR struct module_s *modp, 
+		        FAR struct mod_loadinfo_s *loadinfo, 
                         FAR Elf_Shdr *shdr,
                         FAR Elf_Sym *sym);
 

libs/libc/modlib/modlib_loadhdrs.c
@@ -70,6 +70,14 @@ int modlib_loadhdrs(FAR struct mod_loadinfo_s *loadinfo)
       return -EINVAL;
     }
 
+  /* Verify that there are program headers */
+
+  if (loadinfo->ehdr.e_phnum < 1)
+    {
+      berr("ERROR: No program headers(?)\n");
+      return -EINVAL;
+    }
+
   /* Get the total size of the section header table */
 
   shdrsize = (size_t)loadinfo->ehdr.e_shentsize *
@@ -80,9 +88,19 @@ int modlib_loadhdrs(FAR struct mod_loadinfo_s *loadinfo)
       return -ESPIPE;
     }
 
+  /* Get the total size of the program header table */
+
+  phdrsize = (size_t)loadinfo->ehdr.e_phentsize *
+             (size_t)loadinfo->ehdr.e_phnum;
+  if (loadinfo->ehdr.e_phoff + phdrsize > loadinfo->filelen)
+    {
+      berr("ERROR: Insufficent space in file for program header table\n");
+      return -ESPIPE;
+    }
+
   /* Allocate memory to hold a working copy of the sector header table */
 
-  loadinfo->shdr = (FAR Elf_Shdr *)lib_malloc(shdrsize);
+  loadinfo->shdr = (FAR FAR Elf_Shdr *)lib_malloc(shdrsize);
   if (!loadinfo->shdr)
     {
       berr("ERROR: Failed to allocate the section header table. Size: %ld\n",
@@ -99,43 +117,26 @@ int modlib_loadhdrs(FAR struct mod_loadinfo_s *loadinfo)
       berr("ERROR: Failed to read section header table: %d\n", ret);
     }
 
-  if (loadinfo->ehdr.e_phnum > 0)
+  /* Allocate memory to hold a working copy of the program header table */
+
+  loadinfo->phdr = (FAR FAR Elf_Phdr *)lib_malloc(phdrsize);
+  if (!loadinfo->shdr)
     {
-      /* Get the total size of the program header table */
-
-      phdrsize = (size_t)loadinfo->ehdr.e_phentsize *
-                 (size_t)loadinfo->ehdr.e_phnum;
-      if (loadinfo->ehdr.e_phoff + phdrsize > loadinfo->filelen)
-        {
-          berr("ERROR: Insufficent space for program header table\n");
-          return -ESPIPE;
-        }
-
-      /* Allocate memory to hold a working copy of the program header table */
-
-      loadinfo->phdr = (FAR Elf_Phdr *)lib_malloc(phdrsize);
-      if (!loadinfo->phdr)
-        {
-          lib_free((FAR void *)loadinfo->shdr);
-          berr("ERROR: Failed to allocate the program header table."
-               "Size: %ld\n", (long)phdrsize);
-          return -ENOMEM;
-        }
-
-      /* Read the program header table into memory */
-
-      ret = modlib_read(loadinfo, (FAR uint8_t *)loadinfo->phdr, phdrsize,
-                        loadinfo->ehdr.e_phoff);
-      if (ret < 0)
-        {
-          berr("ERROR: Failed to read program header table: %d\n", ret);
-          lib_free((FAR void *)loadinfo->phdr);
-          lib_free((FAR void *)loadinfo->shdr);
-        }
+      lib_free((FAR void *)loadinfo->shdr);
+      berr("ERROR: Failed to allocate the program header table. Size: %ld\n",
+           (long)phdrsize);
+      return -ENOMEM;
     }
-  else
+
+  /* Read the program header table into memory */
+
+  ret = modlib_read(loadinfo, (FAR uint8_t *)loadinfo->phdr, phdrsize,
+                    loadinfo->ehdr.e_phoff);
+  if (ret < 0)
     {
-      loadinfo->phdr = NULL;
+      berr("ERROR: Failed to read program header table: %d\n", ret);
+      lib_free((FAR void *)loadinfo->phdr);
+      lib_free((FAR void *)loadinfo->shdr);
     }
 
   return ret;