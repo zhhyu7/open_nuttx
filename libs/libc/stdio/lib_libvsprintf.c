apache/nuttx

lib_libvsprintf.c:add option in Kconfig to control "%p*" special format specifier.
Signed-off-by: likun17 <likun17@xiaomi.com>

libs/libc/stdio/lib_libvsprintf.c
@@ -1114,6 +1114,7 @@ str_lpad:
               break;
 
             case 'p':
+#ifdef CONFIG_LIBC_PRINT_EXTENSION
               c = fmt_char(fmt);
               switch (c)
                 {
@@ -1128,22 +1129,22 @@ str_lpad:
                   case 'V':
                     {
                       FAR struct va_format *vaf = (FAR void *)(uintptr_t)x;
-#ifdef va_copy
+#  ifdef va_copy
                       va_list copy;
 
                       va_copy(copy, *vaf->va);
                       lib_vsprintf(stream, vaf->fmt, copy);
                       va_end(copy);
-#else
+#  else
                       lib_vsprintf(stream, vaf->fmt, *vaf->va);
-#endif
+#  endif
                       continue;
                     }
 
                   case 'S':
                   case 's':
                     {
-#ifdef CONFIG_ALLSYMS
+#  ifdef CONFIG_ALLSYMS
                       FAR const struct symtab_s *symbol;
                       FAR void *addr = (FAR void *)(uintptr_t)x;
                       size_t symbolsize;
@@ -1168,7 +1169,7 @@ str_lpad:
 
                           continue;
                         }
-#endif
+#  endif
                       break;
                     }
 
@@ -1176,6 +1177,7 @@ str_lpad:
                     fmt_ungetc(fmt);
                     break;
                 }
+#endif
 
               flags |= FL_ALT;
 

zhhyu7/open_nuttx

Change-Id: I83f23de1fb6345fece0b9a101df5309c4533d8d1

libs/libc/stdio/lib_libvsprintf.c
@@ -1121,6 +1121,7 @@ str_lpad:
               break;
 
             case 'p':
+#ifdef LIBC_PRINT_EXTENSION
               c = fmt_char(fmt);
               switch (c)
                 {
@@ -1135,22 +1136,22 @@ str_lpad:
                   case 'V':
                     {
                       FAR struct va_format *vaf = (FAR void *)(uintptr_t)x;
-#ifdef va_copy
+#  ifdef va_copy
                       va_list copy;
 
                       va_copy(copy, *vaf->va);
                       lib_vsprintf(stream, vaf->fmt, copy);
                       va_end(copy);
-#else
+#  else
                       lib_vsprintf(stream, vaf->fmt, *vaf->va);
-#endif
+#  endif
                       continue;
                     }
 
                   case 'S':
                   case 's':
                     {
-#ifdef CONFIG_ALLSYMS
+#  ifdef CONFIG_ALLSYMS
                       FAR const struct symtab_s *symbol;
                       FAR void *addr = (FAR void *)(uintptr_t)x;
                       size_t symbolsize;
@@ -1175,7 +1176,7 @@ str_lpad:
 
                           continue;
                         }
-#endif
+#  endif
                       break;
                     }
 
@@ -1183,6 +1184,7 @@ str_lpad:
                     fmt_ungetc(fmt);
                     break;
                 }
+#endif
 
               flags |= FL_ALT;
 