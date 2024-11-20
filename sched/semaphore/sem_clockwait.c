apache/nuttx

sched: change xx_timeout param from pid to tcb
Signed-off-by: ligd <liguiding1@xiaomi.com>

sched/semaphore/sem_clockwait.c
@@ -151,7 +151,7 @@ int nxsem_clockwait(FAR sem_t *sem, clockid_t clockid,
 
   /* Start the watchdog */
 
-  wd_start(&rtcb->waitdog, ticks, nxsem_timeout, nxsched_gettid());
+  wd_start(&rtcb->waitdog, ticks, nxsem_timeout, (uintptr_t)rtcb);
 
   /* Now perform the blocking wait.  If nxsem_wait() fails, the
    * negated errno value will be returned below.

zhhyu7/open_nuttx

Change-Id: I08e0ebfa2a12f28751a7b6c809f97b405233b1ea

sched/semaphore/sem_clockwait.c
@@ -150,7 +150,7 @@ int nxsem_clockwait(FAR sem_t *sem, clockid_t clockid,
 
   /* Start the watchdog */
 
-  wd_start(&rtcb->waitdog, ticks, nxsem_timeout, nxsched_gettid());
+  wd_start(&rtcb->waitdog, ticks, nxsem_timeout, (wdparm_t)rtcb);
 
   /* Now perform the blocking wait.  If nxsem_wait() fails, the
    * negated errno value will be returned below.