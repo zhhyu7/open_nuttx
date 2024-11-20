apache/nuttx

compile: add DEBUG_SYMBOLS_LEVEL allow custom the level
Signed-off-by: ligd <liguiding1@xiaomi.com>

arch/arm64/src/cmake/Toolchain.cmake
@@ -172,7 +172,7 @@ if(CONFIG_DEBUG_LINK_MAP)
 endif()
 
 if(CONFIG_DEBUG_SYMBOLS)
-  add_compile_options(-g3)
+  add_compile_options(${CONFIG_DEBUG_SYMBOLS_LEVEL})
 endif()
 
 if(CONFIG_ARCH_TOOLCHAIN_GNU)

arch/risc-v/src/cmake/Toolchain.cmake
@@ -220,7 +220,7 @@ if(CONFIG_DEBUG_LINK_MAP)
 endif()
 
 if(CONFIG_DEBUG_SYMBOLS)
-  add_compile_options(-g3)
+  add_compile_options(${CONFIG_DEBUG_SYMBOLS_LEVEL})
 endif()
 
 # Generic GNU RVG toolchain

arch/sim/src/cmake/Toolchain.cmake
@@ -30,7 +30,7 @@ endif()
 add_compile_options(-fno-common)
 
 if(CONFIG_DEBUG_SYMBOLS)
-  add_compile_options(-g3)
+  add_compile_options(${CONFIG_DEBUG_SYMBOLS_LEVEL})
 endif()
 
 if(CONFIG_SIM_M32)

arch/x86_64/src/cmake/Toolchain.cmake
@@ -78,7 +78,7 @@ if(CONFIG_ARCH_COVERAGE)
 endif()
 
 if(CONFIG_DEBUG_SYMBOLS)
-  add_compile_options(-g3)
+  add_compile_options(${CONFIG_DEBUG_SYMBOLS_LEVEL})
 endif()
 
 # Architecture flags

zhhyu7/open_nuttx

Change-Id: I61d572ed5ac6ecd8309da889f39be342cadf4654

arch/arm64/src/cmake/Toolchain.cmake
@@ -156,7 +156,7 @@ if(CONFIG_DEBUG_LINK_MAP)
 endif()
 
 if(CONFIG_DEBUG_SYMBOLS)
-  add_compile_options(-g3)
+  add_compile_options(${CONFG_DEBUG_SYMBOLS_LEVEL})
 endif()
 
 if(CONFIG_ARCH_TOOLCHAIN_GNU)

arch/risc-v/src/cmake/Toolchain.cmake
@@ -162,7 +162,7 @@ if(CONFIG_DEBUG_LINK_MAP)
 endif()
 
 if(CONFIG_DEBUG_SYMBOLS)
-  add_compile_options(-g3)
+  add_compile_options(${CONFG_DEBUG_SYMBOLS_LEVEL})
 endif()
 
 # Generic GNU RVG toolchain

arch/sim/src/cmake/Toolchain.cmake
@@ -44,7 +44,7 @@ add_compile_options(
   -U__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__)
 
 if(CONFIG_DEBUG_SYMBOLS)
-  add_compile_options(-g3)
+  add_compile_options(${CONFG_DEBUG_SYMBOLS_LEVEL})
 endif()
 
 if(CONFIG_DEBUG_CUSTOMOPT)

arch/x86_64/src/cmake/Toolchain.cmake
@@ -71,7 +71,7 @@ if(CONFIG_SCHED_GCOV)
 endif()
 
 if(CONFIG_DEBUG_SYMBOLS)
-  add_compile_options(-g3)
+  add_compile_options(${CONFG_DEBUG_SYMBOLS_LEVEL})
 endif()
 
 # Architecture flags