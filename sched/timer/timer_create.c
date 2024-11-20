apache/nuttx

signal: Add support for SIGEV_THREAD_ID and sigev_notify_thread_id
This patch added support for SIGEV_THREAD_ID and sigev_notify_thread_id.

Signed-off-by: ouyangxiangzhen <ouyangxiangzhen@xiaomi.com>

sched/timer/timer_create.c
@@ -158,6 +158,7 @@ int timer_create(clockid_t clockid, FAR struct sigevent *evp,
                  FAR timer_t *timerid)
 {
   FAR struct posix_timer_s *ret;
+  FAR struct tcb_s *tcb = nxsched_self();
 
   /* Sanity checks. */
 
@@ -181,13 +182,32 @@ int timer_create(clockid_t clockid, FAR struct sigevent *evp,
 
   ret->pt_clock = clockid;
   ret->pt_crefs = 1;
-  ret->pt_owner = nxsched_getpid();
+  ret->pt_owner = tcb->pid;
   ret->pt_delay = 0;
 
   /* Was a struct sigevent provided? */
 
   if (evp)
     {
+      FAR struct tcb_s *ntcb;
+
+      /* Check the SIGEV_THREAD_ID and validate the tid */
+
+      if (evp->sigev_notify & SIGEV_THREAD_ID)
+        {
+          /* Make sure that the notified thread is
+           * in same process with current thread.
+           */
+
+          ntcb = nxsched_get_tcb(evp->sigev_notify_thread_id);
+
+          if (ntcb == NULL || tcb->group != ntcb->group)
+            {
+              set_errno(EINVAL);
+              return ERROR;
+            }
+        }
+
       /* Yes, copy the entire struct sigevent content */
 
       memcpy(&ret->pt_event, evp, sizeof(struct sigevent));

zhhyu7/open_nuttx

Change-Id: Ia094e10899fe5b199dd6b34e0dfb1a27082f882a

sched/timer/timer_create.c
@@ -37,6 +37,7 @@
 #include <nuttx/kmalloc.h>
 #include <nuttx/spinlock.h>
 
+#include "sched/sched.h"
 #include "timer/timer.h"
 
 #ifndef CONFIG_DISABLE_POSIX_TIMERS
@@ -158,6 +159,7 @@ int timer_create(clockid_t clockid, FAR struct sigevent *evp,
                  FAR timer_t *timerid)
 {
   FAR struct posix_timer_s *ret;
+  FAR struct tcb_s *tcb = this_task();
 
   /* Sanity checks. */
 
@@ -181,7 +183,7 @@ int timer_create(clockid_t clockid, FAR struct sigevent *evp,
 
   ret->pt_clock = clockid;
   ret->pt_crefs = 1;
-  ret->pt_owner = nxsched_getpid();
+  ret->pt_owner = tcb->pid;
   ret->pt_delay = 0;
   ret->pt_expected = 0;
 
@@ -189,6 +191,25 @@ int timer_create(clockid_t clockid, FAR struct sigevent *evp,
 
   if (evp)
     {
+      FAR struct tcb_s *ntcb;
+
+      /* Check the SIGEV_THREAD_ID and validate the tid */
+
+      if (evp->sigev_notify & SIGEV_THREAD_ID)
+        {
+          /* Make sure that the notified thread is
+           * in same process with current thread.
+           */
+
+          ntcb = nxsched_get_tcb(evp->sigev_notify_thread_id);
+
+          if (ntcb == NULL || tcb->group != ntcb->group)
+            {
+              set_errno(EINVAL);
+              return ERROR;
+            }
+        }
+
       /* Yes, copy the entire struct sigevent content */
 
       memcpy(&ret->pt_event, evp, sizeof(struct sigevent));