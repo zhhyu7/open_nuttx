apache/nuttx

libc:Add macro restrictions to code that uses floating point numbers
Signed-off-by: anjiahao <anjiahao@xiaomi.com>

libs/libc/wchar/lib_wcstof.c
@@ -48,7 +48,9 @@
  *
  ****************************************************************************/
 
+#ifdef CONFIG_HAVE_FLOAT
 float wcstof(FAR const wchar_t *nptr, FAR wchar_t **endptr)
 {
   return strtof((FAR const char *)nptr, (FAR char **)endptr);
 }
+#endif

zhhyu7/open_nuttx

Change-Id: If7d0dc773dd5288f3d88bcd6cbc99ce700636c8a

libs/libc/wchar/lib_wcstof.c
@@ -48,7 +48,9 @@
  *
  ****************************************************************************/
 
+#ifdef CONFIG_HAVE_FLAOT
 float wcstof(FAR const wchar_t *nptr, FAR wchar_t **endptr)
 {
   return strtof((FAR const char *)nptr, (FAR char **)endptr);
 }
+#endif