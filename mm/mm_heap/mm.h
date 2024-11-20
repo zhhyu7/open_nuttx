apache/nuttx

mm: Terminate the backtrace array with one NULL pointer
since one entry is enough to identify the end of back trace

Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
Change-Id: I91521955418dd69b8e5877caa7192a9992c63a7f

mm/mm_heap/mm.h
@@ -89,9 +89,9 @@
          if ((heap)->mm_procfs.backtrace || (tcb && tcb->flags & TCB_FLAG_HEAP_DUMP)) \
            { \
              int result = backtrace(tmp->backtrace, CONFIG_MM_BACKTRACE); \
-             while (result < CONFIG_MM_BACKTRACE) \
+             if (result < CONFIG_MM_BACKTRACE) \
                { \
-                 tmp->backtrace[result++] = NULL; \
+                 tmp->backtrace[result] = NULL; \
                } \
            } \
          else \

zhhyu7/open_nuttx

Change-Id: I79255a3fb748548e05cd3e89b2c34f1cfec9eeb4

mm/mm_heap/mm.h
@@ -89,14 +89,14 @@
          if ((heap)->mm_procfs.backtrace || (tcb && tcb->flags & TCB_FLAG_HEAP_DUMP)) \
            { \
              int n = backtrace(tmp->backtrace, CONFIG_MM_BACKTRACE); \
-             while (n < CONFIG_MM_BACKTRACE) \
+             if (n < CONFIG_MM_BACKTRACE) \
                { \
-                 tmp->backtrace[n++] = NULL; \
+                 tmp->backtrace[n] = NULL; \
                } \
            } \
          else \
            { \
-             tmp->backtrace[0] = 0; \
+             tmp->backtrace[0] = NULL; \
            } \
        } \
      while (0)