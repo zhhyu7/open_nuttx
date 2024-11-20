hujun260/nuttx

memepool:fix memory consumption double counting issue
VELAPLATFO-3941

Signed-off-by: anjiahao <anjiahao@xiaomi.com>
Change-Id: I2a8671c30be74304b29f30d784d6f59d4c06f996

mm/mm_heap/mm_initialize.c
@@ -42,6 +42,40 @@
 #endif
 
 /****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+#if CONFIG_MM_HEAP_MEMPOOL_THRESHOLD != 0 && CONFIG_MM_BACKTRACE >= 0
+
+/****************************************************************************
+ * Name: mempool_memalign
+ *
+ * Description:
+ *   This function call mm_memalign and set mm_backtrace pid to free pid
+ *   avoid repeated calculation.
+ ****************************************************************************/
+
+static FAR void *mempool_memalign(FAR void *arg, size_t alignment,
+                                  size_t size)
+{
+  FAR struct mm_allocnode_s *node;
+  FAR void *ret;
+
+  ret = mm_memalign(arg, alignment, size);
+  if (ret)
+    {
+      node = (FAR struct mm_allocnode_s *)
+      ((FAR char *)ret - SIZEOF_MM_ALLOCNODE);
+      node->pid = MM_BACKTRACE_FREE_PID;
+    }
+
+  return ret;
+}
+#else
+#  define mempool_memalign mm_memalign
+#endif
+
+/****************************************************************************
  * Public Functions
  ****************************************************************************/
 
@@ -254,11 +288,10 @@ FAR struct mm_heap_s *mm_initialize(FAR const char *name,
     }
 
   heap->mm_mpool = mempool_multiple_init(name, poolsize, MEMPOOL_NPOOLS,
-                                  (mempool_multiple_alloc_t)mm_memalign,
+                                  (mempool_multiple_alloc_t)mempool_memalign,
                                   (mempool_multiple_free_t)mm_free, heap,
                                   CONFIG_MM_HEAP_MEMPOOL_EXPAND,
-                                  CONFIG_MM_HEAP_MEMPOOL_DICTIONARY_EXPAND,
-                                  true);
+                                  CONFIG_MM_HEAP_MEMPOOL_DICTIONARY_EXPAND);
 #endif
 
   return heap;

zhhyu7/open_nuttx

Change-Id: I7fadd5566bc312a40dfd54116a6b47809c6c8678

mm/mm_heap/mm_initialize.c
@@ -42,6 +42,40 @@
 #endif
 
 /****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+#if CONFIG_MM_HEAP_MEMPOOL_THRESHOLD != 0 && CONFIG_MM_BACKTRACE >= 0
+
+/****************************************************************************
+ * Name: mempool_memalign
+ *
+ * Description:
+ *   This function call mm_memalign and set mm_backtrace pid to free pid
+ *   avoid repeated calculation.
+ ****************************************************************************/
+
+static FAR void *mempool_memalign(FAR void *arg, size_t alignment,
+                                  size_t size)
+{
+  FAR struct mm_allocnode_s *node;
+  FAR void *ret;
+
+  ret = mm_memalign(arg, alignment, size);
+  if (ret)
+    {
+      node = (FAR struct mm_allocnode_s *)
+      ((FAR char *)ret - SIZEOF_MM_ALLOCNODE);
+      node->pid = MM_BACKTRACE_MEMPOOL_PID;
+    }
+
+  return ret;
+}
+#else
+#  define mempool_memalign mm_memalign
+#endif
+
+/****************************************************************************
  * Public Functions
  ****************************************************************************/
 
@@ -253,11 +287,10 @@ FAR struct mm_heap_s *mm_initialize(FAR const char *name,
     }
 
   heap->mm_mpool = mempool_multiple_init(name, poolsize, MEMPOOL_NPOOLS,
-                                  (mempool_multiple_alloc_t)mm_memalign,
+                                  (mempool_multiple_alloc_t)mempool_memalign,
                                   (mempool_multiple_free_t)mm_free, heap,
                                   CONFIG_MM_HEAP_MEMPOOL_EXPAND,
-                                  CONFIG_MM_HEAP_MEMPOOL_DICTIONARY_EXPAND,
-                                  true);
+                                  CONFIG_MM_HEAP_MEMPOOL_DICTIONARY_EXPAND);
 #endif
 
   return heap;