apache/nuttx

mqueue: add file_mq_xx for kernel use
Change-Id: Ida12f5938388cca2f233a4cde90277a218033645
Signed-off-by: ligd <liguiding1@xiaomi.com>

arch/xtensa/src/esp32/esp32_wifi_adapter.c
@@ -88,9 +88,9 @@ struct irq_adpt
 
 struct mq_adpt
 {
-  mqd_t    mq;              /* Message queue handle */
-  uint32_t msgsize;         /* Message size */
-  char     name[16];        /* Message queue name */
+  struct file mq;           /* Message queue handle */
+  uint32_t    msgsize;      /* Message size */
+  char        name[16];     /* Message queue name */
 };
 
 /* WiFi time private data */
@@ -1254,8 +1254,8 @@ static int32_t esp_mutex_unlock(void *mutex_data)
 static void *esp_queue_create(uint32_t queue_len, uint32_t item_size)
 {
   struct mq_attr attr;
-  mqd_t mq;
   struct mq_adpt *mq_adpt;
+  int ret;
 
   mq_adpt = kmm_malloc(sizeof(struct mq_adpt));
   if (!mq_adpt)
@@ -1272,15 +1272,15 @@ static void *esp_queue_create(uint32_t queue_len, uint32_t item_size)
   attr.mq_curmsgs = 0;
   attr.mq_flags   = 0;
 
-  mq = mq_open(mq_adpt->name, O_RDWR | O_CREAT, 0644, &attr);
-  if (!mq)
+  ret = file_mq_open(&mq_adpt->mq, mq_adpt->name,
+                     O_RDWR | O_CREAT, 0644, &attr);
+  if (ret < 0)
     {
       wlerr("ERROR: Failed to create mqueue\n");
       kmm_free(mq_adpt);
       return NULL;
     }
 
-  mq_adpt->mq = mq;
   mq_adpt->msgsize = item_size;
 
   return (void *)mq_adpt;
@@ -1304,7 +1304,7 @@ static void esp_queue_delete(void *queue)
 {
   struct mq_adpt *mq_adpt = (struct mq_adpt *)queue;
 
-  mq_close(mq_adpt->mq);
+  file_mq_close(&mq_adpt->mq);
   mq_unlink(mq_adpt->name);
   kmm_free(mq_adpt);
 }
@@ -1341,8 +1341,8 @@ static int32_t esp_queue_send_generic(void *queue, void *item,
        * instead of application API
        */
 
-      ret = nxmq_send(mq_adpt->mq, (const char *)item,
-                    mq_adpt->msgsize, prio);
+      ret = file_mq_send(&mq_adpt->mq, (const char *)item,
+                         mq_adpt->msgsize, prio);
       if (ret < 0)
         {
           wlerr("ERROR: Failed to send message to mqueue error=%d\n",
@@ -1363,8 +1363,8 @@ static int32_t esp_queue_send_generic(void *queue, void *item,
           esp_update_time(&timeout, ticks);
         }
 
-      ret = mq_timedsend(mq_adpt->mq, (const char *)item,
-                         mq_adpt->msgsize, prio, &timeout);
+      ret = file_mq_timedsend(&mq_adpt->mq, (const char *)item,
+                              mq_adpt->msgsize, prio, &timeout);
       if (ret < 0)
         {
           wlerr("ERROR: Failed to timedsend message to mqueue error=%d\n",
@@ -1491,8 +1491,8 @@ static int32_t esp_queue_recv(void *queue, void *item, uint32_t ticks)
 
   if (ticks == OSI_FUNCS_TIME_BLOCKING)
     {
-      ret = mq_receive(mq_adpt->mq, (char *)item,
-                       mq_adpt->msgsize, &prio);
+      ret = file_mq_receive(&mq_adpt->mq, (char *)item,
+                            mq_adpt->msgsize, &prio);
       if (ret < 0)
         {
           wlerr("ERROR: Failed to receive from mqueue error=%d\n", ret);
@@ -1512,8 +1512,8 @@ static int32_t esp_queue_recv(void *queue, void *item, uint32_t ticks)
           esp_update_time(&timeout, ticks);
         }
 
-      ret = mq_timedreceive(mq_adpt->mq, (char *)item,
-                            mq_adpt->msgsize, &prio, &timeout);
+      ret = file_mq_timedreceive(&mq_adpt->mq, (char *)item,
+                                 mq_adpt->msgsize, &prio, &timeout);
       if (ret < 0)
         {
           wlerr("ERROR: Failed to timedreceive from mqueue error=%d\n",
@@ -1544,7 +1544,7 @@ static uint32_t esp_queue_msg_waiting(void *queue)
   struct mq_attr attr;
   struct mq_adpt *mq_adpt = (struct mq_adpt *)queue;
 
-  ret = mq_getattr(mq_adpt->mq, &attr);
+  ret = mq_getattr(&mq_adpt->mq, &attr);
   if (ret < 0)
     {
       wlerr("ERROR: Failed to get attr from mqueue error=%d\n", ret);

wireless/bluetooth/bt_conn.c
@@ -166,7 +166,7 @@ static int conn_tx_kthread(int argc, FAR char *argv[])
 
       /* Get next ACL packet for connection */
 
-      ret = bt_queue_receive(conn->tx_queue, &buf);
+      ret = bt_queue_receive(&conn->tx_queue, &buf);
       DEBUGASSERT(ret >= 0 && buf != NULL);
       UNUSED(ret);
 
@@ -195,7 +195,7 @@ static int conn_tx_kthread(int argc, FAR char *argv[])
        * result in a successful termination of this thread.
        */
 
-      ret = mq_getattr(conn->tx_queue, &attr);
+      ret = mq_getattr(&conn->tx_queue, &attr);
       if (ret != OK)
         {
           break;
@@ -206,7 +206,7 @@ static int conn_tx_kthread(int argc, FAR char *argv[])
           break;
         }
 
-      ret = bt_queue_receive(conn->tx_queue, &buf);
+      ret = bt_queue_receive(&conn->tx_queue, &buf);
       if (ret >= 0)
         {
           DEBUGASSERT(buf != NULL);
@@ -470,7 +470,7 @@ void bt_conn_send(FAR struct bt_conn_s *conn, FAR struct bt_buf_s *buf)
 
   while ((buf = (FAR struct bt_buf_s *)sq_remfirst(&fraglist)) != NULL)
     {
-      bt_queue_send(conn->tx_queue, buf, BT_NORMAL_PRIO);
+      bt_queue_send(&conn->tx_queue, buf, BT_NORMAL_PRIO);
     }
 }
 
@@ -569,7 +569,7 @@ void bt_conn_set_state(FAR struct bt_conn_s *conn,
           ret = bt_queue_open(BT_CONN_TX, O_RDWR | O_CREAT,
                               CONFIG_BLUETOOTH_TXCONN_NMSGS,
                               &conn->tx_queue);
-          DEBUGASSERT(ret >= 0 && g_btdev.tx_queue != 0);
+          DEBUGASSERT(ret >= 0);
           UNUSED(ret);
 
           /* Get exclusive access to the handoff structure.  The count will
@@ -609,7 +609,7 @@ void bt_conn_set_state(FAR struct bt_conn_s *conn,
         if (old_state == BT_CONN_CONNECTED ||
            old_state == BT_CONN_DISCONNECT)
           {
-            bt_queue_send(conn->tx_queue, bt_buf_alloc(BT_DUMMY, NULL, 0),
+            bt_queue_send(&conn->tx_queue, bt_buf_alloc(BT_DUMMY, NULL, 0),
                           BT_NORMAL_PRIO);
           }
 

zhhyu7/open_nuttx

Change-Id: Ic24f93954361cfe0066579e3003a69991e7c2c18

arch/xtensa/src/esp32/esp32_wifi_adapter.c
@@ -29,7 +29,6 @@
 #include <string.h>
 #include <assert.h>
 #include <pthread.h>
-#include <mqueue.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <clock/clock.h>
@@ -37,6 +36,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include "nuttx/kmalloc.h"
+#include <nuttx/mqueue.h>
 #include "nuttx/spinlock.h"
 #include <nuttx/irq.h>
 #include <nuttx/semaphore.h>
@@ -88,9 +88,9 @@ struct irq_adpt
 
 struct mq_adpt
 {
-  mqd_t    mq;              /* Message queue handle */
-  uint32_t msgsize;         /* Message size */
-  char     name[16];        /* Message queue name */
+  struct file mq;           /* Message queue handle */
+  uint32_t    msgsize;      /* Message size */
+  char        name[16];     /* Message queue name */
 };
 
 /* WiFi time private data */
@@ -1254,8 +1254,8 @@ static int32_t esp_mutex_unlock(void *mutex_data)
 static void *esp_queue_create(uint32_t queue_len, uint32_t item_size)
 {
   struct mq_attr attr;
-  mqd_t mq;
   struct mq_adpt *mq_adpt;
+  int ret;
 
   mq_adpt = kmm_malloc(sizeof(struct mq_adpt));
   if (!mq_adpt)
@@ -1272,15 +1272,15 @@ static void *esp_queue_create(uint32_t queue_len, uint32_t item_size)
   attr.mq_curmsgs = 0;
   attr.mq_flags   = 0;
 
-  mq = mq_open(mq_adpt->name, O_RDWR | O_CREAT, 0644, &attr);
-  if (!mq)
+  ret = file_mq_open(&mq_adpt->mq, mq_adpt->name,
+                     O_RDWR | O_CREAT, 0644, &attr);
+  if (ret < 0)
     {
       wlerr("ERROR: Failed to create mqueue\n");
       kmm_free(mq_adpt);
       return NULL;
     }
 
-  mq_adpt->mq = mq;
   mq_adpt->msgsize = item_size;
 
   return (void *)mq_adpt;
@@ -1304,8 +1304,8 @@ static void esp_queue_delete(void *queue)
 {
   struct mq_adpt *mq_adpt = (struct mq_adpt *)queue;
 
-  mq_close(mq_adpt->mq);
-  mq_unlink(mq_adpt->name);
+  file_mq_close(&mq_adpt->mq);
+  file_mq_unlink(mq_adpt->name);
   kmm_free(mq_adpt);
 }
 
@@ -1341,8 +1341,8 @@ static int32_t esp_queue_send_generic(void *queue, void *item,
        * instead of application API
        */
 
-      ret = nxmq_send(mq_adpt->mq, (const char *)item,
-                    mq_adpt->msgsize, prio);
+      ret = file_mq_send(&mq_adpt->mq, (const char *)item,
+                         mq_adpt->msgsize, prio);
       if (ret < 0)
         {
           wlerr("ERROR: Failed to send message to mqueue error=%d\n",
@@ -1363,8 +1363,8 @@ static int32_t esp_queue_send_generic(void *queue, void *item,
           esp_update_time(&timeout, ticks);
         }
 
-      ret = mq_timedsend(mq_adpt->mq, (const char *)item,
-                         mq_adpt->msgsize, prio, &timeout);
+      ret = file_mq_timedsend(&mq_adpt->mq, (const char *)item,
+                              mq_adpt->msgsize, prio, &timeout);
       if (ret < 0)
         {
           wlerr("ERROR: Failed to timedsend message to mqueue error=%d\n",
@@ -1491,8 +1491,8 @@ static int32_t esp_queue_recv(void *queue, void *item, uint32_t ticks)
 
   if (ticks == OSI_FUNCS_TIME_BLOCKING)
     {
-      ret = mq_receive(mq_adpt->mq, (char *)item,
-                       mq_adpt->msgsize, &prio);
+      ret = file_mq_receive(&mq_adpt->mq, (char *)item,
+                            mq_adpt->msgsize, &prio);
       if (ret < 0)
         {
           wlerr("ERROR: Failed to receive from mqueue error=%d\n", ret);
@@ -1512,8 +1512,8 @@ static int32_t esp_queue_recv(void *queue, void *item, uint32_t ticks)
           esp_update_time(&timeout, ticks);
         }
 
-      ret = mq_timedreceive(mq_adpt->mq, (char *)item,
-                            mq_adpt->msgsize, &prio, &timeout);
+      ret = file_mq_timedreceive(&mq_adpt->mq, (char *)item,
+                                 mq_adpt->msgsize, &prio, &timeout);
       if (ret < 0)
         {
           wlerr("ERROR: Failed to timedreceive from mqueue error=%d\n",
@@ -1544,7 +1544,7 @@ static uint32_t esp_queue_msg_waiting(void *queue)
   struct mq_attr attr;
   struct mq_adpt *mq_adpt = (struct mq_adpt *)queue;
 
-  ret = mq_getattr(mq_adpt->mq, &attr);
+  ret = file_mq_getattr(&mq_adpt->mq, &attr);
   if (ret < 0)
     {
       wlerr("ERROR: Failed to get attr from mqueue error=%d\n", ret);

wireless/bluetooth/bt_conn.c
@@ -166,7 +166,7 @@ static int conn_tx_kthread(int argc, FAR char *argv[])
 
       /* Get next ACL packet for connection */
 
-      ret = bt_queue_receive(conn->tx_queue, &buf);
+      ret = bt_queue_receive(&conn->tx_queue, &buf);
       DEBUGASSERT(ret >= 0 && buf != NULL);
       UNUSED(ret);
 
@@ -195,7 +195,7 @@ static int conn_tx_kthread(int argc, FAR char *argv[])
        * result in a successful termination of this thread.
        */
 
-      ret = mq_getattr(conn->tx_queue, &attr);
+      ret = file_mq_getattr(&conn->tx_queue, &attr);
       if (ret != OK)
         {
           break;
@@ -206,7 +206,7 @@ static int conn_tx_kthread(int argc, FAR char *argv[])
           break;
         }
 
-      ret = bt_queue_receive(conn->tx_queue, &buf);
+      ret = bt_queue_receive(&conn->tx_queue, &buf);
       if (ret >= 0)
         {
           DEBUGASSERT(buf != NULL);
@@ -470,7 +470,7 @@ void bt_conn_send(FAR struct bt_conn_s *conn, FAR struct bt_buf_s *buf)
 
   while ((buf = (FAR struct bt_buf_s *)sq_remfirst(&fraglist)) != NULL)
     {
-      bt_queue_send(conn->tx_queue, buf, BT_NORMAL_PRIO);
+      bt_queue_send(&conn->tx_queue, buf, BT_NORMAL_PRIO);
     }
 }
 
@@ -569,7 +569,7 @@ void bt_conn_set_state(FAR struct bt_conn_s *conn,
           ret = bt_queue_open(BT_CONN_TX, O_RDWR | O_CREAT,
                               CONFIG_BLUETOOTH_TXCONN_NMSGS,
                               &conn->tx_queue);
-          DEBUGASSERT(ret >= 0 && g_btdev.tx_queue != 0);
+          DEBUGASSERT(ret >= 0);
           UNUSED(ret);
 
           /* Get exclusive access to the handoff structure.  The count will
@@ -609,7 +609,7 @@ void bt_conn_set_state(FAR struct bt_conn_s *conn,
         if (old_state == BT_CONN_CONNECTED ||
            old_state == BT_CONN_DISCONNECT)
           {
-            bt_queue_send(conn->tx_queue, bt_buf_alloc(BT_DUMMY, NULL, 0),
+            bt_queue_send(&conn->tx_queue, bt_buf_alloc(BT_DUMMY, NULL, 0),
                           BT_NORMAL_PRIO);
           }
 