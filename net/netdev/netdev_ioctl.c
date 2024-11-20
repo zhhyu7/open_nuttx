apache/nuttx

Add Cellular link layer support
VELAPLATFO-3351

1.Add cellular link layer enum definition and register flow
2.Add ioctl flow to set cellular NICs parameters

Signed-off-by: luojun1 <luojun1@xiaomi.com>
Change-Id: I9b9ead334ff02c59bd9b4acaf34f6ec476ec8908

net/netdev/netdev_ioctl.c
@@ -1112,6 +1112,21 @@ static int netdev_ifr_ioctl(FAR struct socket *psock, int cmd,
         break;
 #endif
 
+#if defined(CONFIG_NETDEV_IOCTL) && defined(CONFIG_NET_CELLULAR)
+      case SIOCSCELLNETDEV:  /* set params for cellular network devices */
+        {
+          dev = netdev_findbyname(req->ifr_name);
+          if (dev && dev->d_ioctl)
+            {
+              struct cell_ioctl_data_s *cell_netdev_data =
+                              &req->ifr_ifru.lifru_cell_data;
+              ret = dev->d_ioctl(dev, cmd,
+                              (unsigned long)(uintptr_t)cell_netdev_data);
+            }
+        }
+        break;
+#endif
+
       default:
         {
           ret = -ENOTTY;

zhhyu7/open_nuttx

Change-Id: I801950ba6dbbdd139fec95099c36d74eabb4e44f

net/netdev/netdev_ioctl.c
@@ -660,7 +660,6 @@ static ssize_t net_ioctl_ifreq_arglen(int cmd)
       case SIOCDCANEXTFILTER:
       case SIOCACANSTDFILTER:
       case SIOCDCANSTDFILTER:
-      case SIOCSCELLNETDEV:
       case SIOCGIFNAME:
       case SIOCGIFINDEX:
         return sizeof(struct ifreq);
@@ -1056,17 +1055,16 @@ static int netdev_ifr_ioctl(FAR struct socket *psock, int cmd,
 
 #if defined(CONFIG_NETDEV_IOCTL) && defined(CONFIG_NET_CELLULAR)
       case SIOCSCELLNETDEV:  /* set params for cellular network devices */
-        if (dev->d_ioctl)
-          {
-            FAR struct cell_ioctl_data_s *cell_netdev_data =
-                            &req->ifr_ifru.ifru_cell_data;
-            ret = dev->d_ioctl(dev, cmd,
-                            (unsigned long)(uintptr_t)cell_netdev_data);
-          }
-        else
-          {
-            ret = -ENOSYS;
-          }
+        {
+          dev = netdev_findbyname(req->ifr_name);
+          if (dev && dev->d_ioctl)
+            {
+              FAR struct cell_ioctl_data_s *cell_netdev_data =
+                              &req->ifr_ifru.ifru_cell_data;
+              ret = dev->d_ioctl(dev, cmd,
+                              (unsigned long)(uintptr_t)cell_netdev_data);
+            }
+        }
         break;
 #endif
 