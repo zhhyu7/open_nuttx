tiiuae/nuttx

arm64/imx9: Add eDMA driver
This driver supports both eDMA3 and eDMA4 (also referred to as DMA0 / DMA1
in some contexts..)

The IP blocks are almost identical, with sufficiently minor differences
to use them via a unified driver. The price is a great amount of code
obfuscation in the hardware description layer.

arch/arm64/include/imx9/chip.h
@@ -31,6 +31,11 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
+/* Cache line sizes (in bytes)for the i.MX9 (Cortex-A55) */
+
+#define ARMV8M_DCACHE_LINESIZE 64  /* 64 bytes (16 words) */
+#define ARMV8M_ICACHE_LINESIZE 64  /* 64 bytes (16 words) */
+
 /* Number of bytes in x kibibytes/mebibytes/gibibytes */
 
 #define KB(x)   ((x) << 10)

zhhyu7/open_nuttx

Change-Id: I485f51a721b148d9afe5751447d6a5ecf64f8b64

arch/arm64/include/imx9/chip.h
@@ -31,6 +31,11 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
+/* Cache line sizes (in bytes)for the i.MX9 (Cortex-A55) */
+
+#define ARMV8A_DCACHE_LINESIZE 64  /* 64 bytes (16 words) */
+#define ARMV8A_ICACHE_LINESIZE 64  /* 64 bytes (16 words) */
+
 /* Number of bytes in x kibibytes/mebibytes/gibibytes */
 
 #define KB(x)   ((x) << 10)