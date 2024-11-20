apache/nuttx

fs/procfs: add StackAlloc field for task stack info
N/A

Change-Id: I4b74191896a4223bb889bbe759694e0b229db71c
Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

fs/procfs/fs_procfsproc.c
@@ -893,9 +893,25 @@ static ssize_t proc_stack(FAR struct proc_file_s *procfile,
   remaining = buflen;
   totalsize = 0;
 
+  /* Show the stack alloc address */
+
+  linesize   = procfs_snprintf(procfile->line, STATUS_LINELEN, "%-12s%p\n",
+                               "StackAlloc:", tcb->stack_alloc_ptr);
+  copysize   = procfs_memcpy(procfile->line, linesize, buffer, remaining,
+                             &offset);
+
+  totalsize += copysize;
+  buffer    += copysize;
+  remaining -= copysize;
+
+  if (totalsize >= buflen)
+    {
+      return totalsize;
+    }
+
   /* Show the stack base address */
 
-  linesize   = procfs_snprintf(procfile->line, STATUS_LINELEN, "%-12s0x%p\n",
+  linesize   = procfs_snprintf(procfile->line, STATUS_LINELEN, "%-12s%p\n",
                                "StackBase:", tcb->stack_base_ptr);
   copysize   = procfs_memcpy(procfile->line, linesize, buffer, remaining,
                              &offset);

zhhyu7/open_nuttx

Change-Id: I9c27a9f6f58d34c02fdb898d8e182eeb9c290500

fs/procfs/fs_procfsproc.c
@@ -854,9 +854,25 @@ static ssize_t proc_stack(FAR struct proc_file_s *procfile,
   remaining = buflen;
   totalsize = 0;
 
+  /* Show the stack alloc address */
+
+  linesize   = snprintf(procfile->line, STATUS_LINELEN, "%-12s%p\n",
+                               "StackAlloc:", tcb->stack_alloc_ptr);
+  copysize   = procfs_memcpy(procfile->line, linesize, buffer, remaining,
+                             &offset);
+
+  totalsize += copysize;
+  buffer    += copysize;
+  remaining -= copysize;
+
+  if (totalsize >= buflen)
+    {
+      return totalsize;
+    }
+
   /* Show the stack base address */
 
-  linesize   = snprintf(procfile->line, STATUS_LINELEN, "%-12s0x%p\n",
+  linesize   = snprintf(procfile->line, STATUS_LINELEN, "%-12s%p\n",
                         "StackBase:", tcb->stack_base_ptr);
   copysize   = procfs_memcpy(procfile->line, linesize, buffer, remaining,
                              &offset);