apache/nuttx

lib_pathbuffer.c:Determine whether to malloc from the heap by CONFIG_LIBC_PATHBUFFER_MALLOC
Summary:
  If we disable LIB_PATHBUFFER_MALLOC, that when the path buffer is insufficient, NULL is returned to avoid applying for a buffer from the heap.

Signed-off-by: chenrun1 <chenrun1@xiaomi.com>

libs/libc/misc/lib_pathbuffer.c
@@ -95,9 +95,15 @@ FAR char *lib_get_pathbuffer(void)
 
   nxmutex_unlock(&g_pathbuffer.lock);
 
-  /* If no free buffer is found, allocate a new one */
+  /* If no free buffer is found, allocate a new one if
+   * CONFIG_LIBC_PATHBUFFER_MALLOC is enabled
+   */
 
+#ifdef CONFIG_LIBC_PATHBUFFER_MALLOC
   return lib_malloc(PATH_MAX);
+#else
+  return NULL;
+#endif
 }
 
 /****************************************************************************
@@ -134,5 +140,7 @@ void lib_put_pathbuffer(FAR char *buffer)
 
   /* Free the buffer if it was dynamically allocated */
 
-  lib_free(buffer);
+#ifdef CONFIG_LIBC_PATHBUFFER_MALLOC
+  return lib_free(buffer);
+#endif
 }

zhhyu7/open_nuttx

Change-Id: Id8b93000dfd5eac9d8c116ecc008bced2b503722

libs/libc/misc/lib_pathbuffer.c
@@ -95,9 +95,15 @@ FAR char *lib_get_pathbuffer(void)
 
   nxmutex_unlock(&g_pathbuffer.lock);
 
-  /* If no free buffer is found, allocate a new one */
+  /* If no free buffer is found, allocate a new one if
+   * CONFIG_LIBC_PATHBUFFER_MALLOC is enabled
+   */
 
+#ifdef CONFIG_LIBC_PATHBUFFER_ALLOCATION
   return lib_malloc(PATH_MAX);
+#else
+  return NULL;
+#endif
 }
 
 /****************************************************************************
@@ -134,5 +140,7 @@ void lib_put_pathbuffer(FAR char *buffer)
 
   /* Free the buffer if it was dynamically allocated */
 
-  lib_free(buffer);
+#ifdef CONFIG_LIBC_PATHBUFFER_ALLOCATION
+  return lib_free(buffer);
+#endif
 }